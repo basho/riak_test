ec612122fae64614041f0d21bcb7ed8f40a4029d
Joe DeVivo <joe.devivo@basho.com>
2012-08-28 10:57:49 -0700

    riak_test runs multiple tests.

--- src/riak_test_runner.erl
+++ src/riak_test_runner.erl
@@ -6,11 +6,39 @@
 %% @doc Runs a module's run/0 function after setting up a log capturing backend for lager. 
 %% It then cleans up that backend and returns the logs as part of the return proplist.
 confirm(TestModule) ->
+    start_lager_backend(),
+    
+    %% Check for api compatibility
+    Result = case proplists:get_value(confirm, 
+                        proplists:get_value(exports, TestModule:module_info()),
+                        -1) of
+        0 ->
+            lager:info("Running Test ~s", [TestModule]), 
+            execute(TestModule);
+        _ ->
+            lager:info("~s is not a runable test", [TestModule]),
+            not_a_runable_test
+    end,
+    
+    lager:info("~s Test Run Complete", [TestModule]),
+    {ok, Log} = stop_lager_backend(),
+    
+    [{test, TestModule}, {status, Result}, {log, Log}].
+    
+    
+start_lager_backend() ->
     LagerLevel = rt:config(rt_lager_level, debug),
-    gen_event:add_handler(lager_event, riak_test_lager_backend, [LagerLevel, false]),
+    gen_event:add_handler(lager_event, riak_test_lager_backend, [LagerLevel, false]).
     
-    Result = TestModule:confirm(),
+stop_lager_backend() ->
+    gen_event:delete_handler(lager_event, riak_test_lager_backend, []).
     
-    {ok, Log} = gen_event:delete_handler(lager_event, riak_test_lager_backend, []),
+execute(TestModule) ->
+    try TestModule:confirm() of
+        ReturnVal -> ReturnVal
+    catch
+        error:Error ->
+            lager:warning("~s failed: ~p", [TestModule, Error]),
+            fail
+    end.
     
-    [{status, Result}, {log, Log}].
