caa99551fd594bb33e38402c815172ef9805c4f6
Joe DeVivo <joe.devivo@basho.com>
2012-08-30 13:42:13 -0400

    Added file output

--- src/riak_test.erl
+++ src/riak_test.erl
@@ -14,13 +14,16 @@ cli_options() ->
  {config,             $c, "conf",             string,           "specifies the project configuration"},
  {tests,              $t, "tests",            string,           "specifies which tests to run"},
  {suites,             $s, "suites",           string,           "which suites to run"},
- {dir,                $d, "dir",              string,           "run all tests in the specified directory"}
+ {dir,                $d, "dir",              string,           "run all tests in the specified directory"},
+ {verbose,            $v, "verbose",          undefined,        "verbose output"},
+ {outdir,             $o, "outdir",           string,           "output directory"}
 ].
 
 
 main(Args) ->
     {ok, {ParsedArgs, HarnessArgs}} = getopt:parse(cli_options(), Args),
         
+    Verbose = proplists:is_defined(verbose, ParsedArgs),
     Config = proplists:get_value(config, ParsedArgs),
     SpecificTests = proplists:get_all_values(tests, ParsedArgs),
     Suites = proplists:get_all_values(suites, ParsedArgs),
@@ -55,33 +58,52 @@ main(Args) ->
     
     %% Start Lager
     application:load(lager),
-    LagerLevel = rt:config(rt_lager_level, debug),
-    application:set_env(lager, handlers, [{lager_console_backend, LagerLevel}]),
+    %% Fileoutput
+    Outdir = proplists:get_value(outdir, ParsedArgs),
+    ConsoleLagerLevel = case Outdir of
+        undefined -> rt:config(rt_lager_level, debug);
+        _ ->
+            filelib:ensure_dir(Outdir),
+            notice
+    end,
+    
+    application:set_env(lager, handlers, [{lager_console_backend, ConsoleLagerLevel}]),
     lager:start(),
     
     %% rt:set_config(rtdev_path, Path),
     %% rt:set_config(rt_max_wait_time, 180000),
     %% rt:set_config(rt_retry_delay, 500),
     %% rt:set_config(rt_harness, rtbe),
-    TestResults = [ run_test(Test, HarnessArgs) || Test <- Tests],
+        
+    TestResults = [ run_test(Test, Outdir, HarnessArgs) || Test <- Tests],
     
-    print_summary(TestResults),
+    print_summary(TestResults, Verbose),
     ok.
 
-run_test(Test, HarnessArgs) ->
+run_test(Test, Outdir, HarnessArgs) ->
     rt:setup_harness(Test, HarnessArgs),
     TestA = list_to_atom(Test),
-    SingleTestResult = riak_test_runner:confirm(TestA),
+    SingleTestResult = riak_test_runner:confirm(TestA, Outdir),
     rt:cleanup_harness(),
     SingleTestResult.
     
-print_summary(TestResults) ->
+print_summary(TestResults, Verbose) ->
     io:format("~nTest Results:~n"),
     
-    Results = [ [ atom_to_list(proplists:get_value(test, SingleTestResult)),
-        proplists:get_value(status, SingleTestResult)] || SingleTestResult <- TestResults],
+    Results = [ 
+                [ atom_to_list(proplists:get_value(test, SingleTestResult)),
+                  proplists:get_value(status, SingleTestResult), 
+                  proplists:get_value(reason, SingleTestResult)] 
+                || SingleTestResult <- TestResults],
     Width = test_name_width(Results),
-    [ io:format("~s: ~s~n", [string:left(Name, Width), Result]) || [Name, Result] <- Results],
+    
+    Print = fun(Test, Status, Reason) ->
+        case {Status, Verbose} of
+            {fail, true} -> io:format("~s: ~s ~p~n", [string:left(Test, Width), Status, Reason]);
+            _ -> io:format("~s: ~s~n", [string:left(Test, Width), Status])
+        end
+    end,
+    [ Print(Test, Status, Reason) || [Test, Status, Reason] <- Results],
     
     PassCount = length(lists:filter(fun(X) -> proplists:get_value(status, X) =:= pass end, TestResults)),
     FailCount = length(lists:filter(fun(X) -> proplists:get_value(status, X) =:= fail end, TestResults)),
