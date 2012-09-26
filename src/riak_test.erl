d4b9e198da625db4ed28d3c3bc2f0e179b3de9bb
Jared Morrow <jared@basho.com>
2012-09-26 12:45:34 -0600

    Add options to --backend flag

--- src/riak_test.erl
+++ src/riak_test.erl
@@ -17,8 +17,8 @@ cli_options() ->
  {dir,                $d, "dir",              string,           "run all tests in the specified directory"},
  {verbose,            $v, "verbose",          undefined,        "verbose output"},
  {outdir,             $o, "outdir",           string,           "output directory"},
- {backend,            $b, "backend",          atom,             "backend to test"},
- {report,             $r, "report",           string,           "you're reporting an official test run, provide platform info (e.g. ubuntu-1204-64"}
+ {backend,            $b, "backend",          atom,             "backend to test [memory | bitcask | eleveldb]"},
+ {report,             $r, "report",           string,           "you're reporting an official test run, provide platform info (e.g. ubuntu-1204-64)"}
 ].
 
 print_help() ->
@@ -35,12 +35,12 @@ main(Args) ->
         {ok, {P, H}} -> {P, H};
         _ -> print_help()
     end,
-    
-    case run_help(ParsedArgs) of 
+
+    case run_help(ParsedArgs) of
         true -> print_help();
         _ -> ok
     end,
-    
+
     %% ibrowse
     application:load(ibrowse),
     application:start(ibrowse),
@@ -57,7 +57,7 @@ main(Args) ->
             filelib:ensure_dir(Outdir),
             notice
     end,
-    
+
     application:set_env(lager, handlers, [{lager_console_backend, ConsoleLagerLevel}]),
     lager:start(),
 
@@ -69,14 +69,14 @@ main(Args) ->
         [] -> ok;
         _ -> io:format("Suites are not currently supported.")
     end,
-    
+
     Version = rt:get_version(),
-    
+
     Backends = case proplists:get_all_values(backend, ParsedArgs) of
         [] -> [bitcask];
         Other -> Other
     end,
-    
+
     Tests = case Report of
         undefined ->
             SpecificTests = proplists:get_all_values(tests, ParsedArgs),
@@ -84,7 +84,7 @@ main(Args) ->
             DirTests = lists:append([load_tests_in_dir(Dir) || Dir <- Dirs]),
             lists:foldl(fun(Test, Tests) ->
                     [{
-                      list_to_atom(Test), 
+                      list_to_atom(Test),
                       [
                           {id, -1},
                           {backend, Backend},
@@ -94,18 +94,18 @@ main(Args) ->
                       ]
                     } || Backend <- Backends ] ++ Tests
                 end, [], lists:usort(DirTests ++ SpecificTests));
-        Platform -> 
+        Platform ->
             giddyup:get_suite(Platform)
     end,
     io:format("Tests to run: ~p~n", [Tests]),
-    
+
     [add_deps(Dep) || Dep <- rt:config(rt_deps)],
     ENode = rt:config(rt_nodename, 'riak_test@127.0.0.1'),
     Cookie = rt:config(rt_cookie, riak),
     [] = os:cmd("epmd -daemon"),
     net_kernel:start([ENode]),
     erlang:set_cookie(node(), Cookie),
-    
+
     TestResults = [ run_test(Test, Outdir, TestMetaData, Report, HarnessArgs) || {Test, TestMetaData} <- Tests],
     print_summary(TestResults, Verbose),
     ok.
@@ -118,18 +118,18 @@ run_test(Test, Outdir, TestMetaData, Report, _HarnessArgs) ->
         _ -> giddyup:post_result(SingleTestResult)
     end,
     SingleTestResult.
-    
+
 print_summary(TestResults, Verbose) ->
     io:format("~nTest Results:~n"),
-    
-    Results = [ 
+
+    Results = [
                 [ atom_to_list(proplists:get_value(test, SingleTestResult)) ++ "-" ++
                   atom_to_list(proplists:get_value(backend, SingleTestResult)),
-                  proplists:get_value(status, SingleTestResult), 
-                  proplists:get_value(reason, SingleTestResult)] 
+                  proplists:get_value(status, SingleTestResult),
+                  proplists:get_value(reason, SingleTestResult)]
                 || SingleTestResult <- TestResults],
     Width = test_name_width(Results),
-    
+
     Print = fun(Test, Status, Reason) ->
         case {Status, Verbose} of
             {fail, true} -> io:format("~s: ~s ~p~n", [string:left(Test, Width), Status, Reason]);
@@ -137,7 +137,7 @@ print_summary(TestResults, Verbose) ->
         end
     end,
     [ Print(Test, Status, Reason) || [Test, Status, Reason] <- Results],
-    
+
     PassCount = length(lists:filter(fun(X) -> proplists:get_value(status, X) =:= pass end, TestResults)),
     FailCount = length(lists:filter(fun(X) -> proplists:get_value(status, X) =:= fail end, TestResults)),
     io:format("---------------------------------------------~n"),
@@ -149,13 +149,13 @@ print_summary(TestResults, Verbose) ->
     end,
     io:format("That's ~w% for those keeping score~n", [Percentage]),
     ok.
-    
+
 test_name_width(Results) ->
     lists:max([ length(X) || [X | _T] <- Results ]).
-    
+
 load_tests_in_dir(Dir) ->
     case filelib:is_dir(Dir) of
-        true -> 
+        true ->
             code:add_path(Dir),
             lists:sort([ string:substr(Filename, 1, length(Filename) - 5) || Filename <- filelib:wildcard("*.beam", Dir)]);
         _ -> io:format("~s is not a dir!~n", [Dir])
