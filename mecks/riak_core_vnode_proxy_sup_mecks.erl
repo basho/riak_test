4c72e13a229a52b005d9b7924f6f307cdca1ed7e
Ryan Zezeski <rzezeski@gmail.com>
2012-11-09 04:16:50 -0500

    Convert gh_riak_core_155 to use new meck system

---
+++ mecks/riak_core_vnode_proxy_sup_mecks.erl
@@ -0,0 +1,15 @@
+-module(riak_core_vnode_proxy_sup_mecks).
+-compile(export_all).
+-include("meck.hrl").
+
+delay_start_proxies(Ms) ->
+    ?M_EXPECT(start_proxies,
+              fun(Mod=riak_kv_vnode) ->
+                      ?M_INFO("Delaying start of riak_kv_vnode proxies for ~pms",
+                              [Ms]),
+                      timer:sleep(Ms),
+                      meck:passthrough([Mod]);
+                 (Mod) ->
+                      meck:passthrough([Mod])
+              end),
+    ok.
--- tests/gh_riak_core_155.erl
+++ tests/gh_riak_core_155.erl
@@ -23,7 +23,7 @@
 
 confirm() ->
     [Node] = rt:build_cluster(1),
-  
+
     %% Generate a valid preflist for our get requests
     rpc:call(Node, riak_core, wait_for_service, [riak_kv]),
     BKey = {<<"bucket">>, <<"value">>},
@@ -40,10 +40,8 @@ confirm() ->
     rt:async_start(Node),
     rt:wait_until_pingable(Node),
 
-    load_code(?MODULE, [Node]),
-    load_code(meck, [Node]),
-    load_code(meck_mod, [Node]),
-    rpc:call(Node, ?MODULE, setup_mocks, []),
+    ok = rpc:call(Node, riak_core_vnode_proxy_sup_mecks, init, []),
+    ok = rpc:call(Node, riak_core_vnode_proxy_sup_mecks, delay_start_proxies, [3000]),
 
     lager:info("Installed mocks to delay riak_kv proxy startup"),
     lager:info("Issuing 10000 gets against ~p", [Node]),
@@ -61,19 +59,6 @@ confirm() ->
 load_code(Module, Nodes) ->
     {Module, Bin, File} = code:get_object_code(Module),
     {_, []} = rpc:multicall(Nodes, code, load_binary, [Module, File, Bin]).
-
-setup_mocks() ->
-    application:start(lager),
-    meck:new(riak_core_vnode_proxy_sup, [unstick, passthrough, no_link]),
-    meck:expect(riak_core_vnode_proxy_sup, start_proxies,
-                fun(Mod=riak_kv_vnode) ->
-                        lager:info("Delaying start of riak_kv_vnode proxies"),
-                        timer:sleep(3000),
-                        meck:passthrough([Mod]);
-                   (Mod) ->
-                        meck:passthrough([Mod])
-                end),
-    lager:info("Installed mocks").
 
 perform_gets(Count, Node, PL, BKey) ->
     rpc:call(Node, riak_kv_vnode, get, [PL, BKey, make_ref()]),
