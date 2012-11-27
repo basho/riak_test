13ee1bb0e9385e94889b0eca1f931956ad4d73a6
Sean Cribbs <sean@basho.com>
2012-11-27 11:23:26 -0500

    Use virtualenv to prevent installation of packages in protected dirs.

--- tests/client_python_verify.erl
+++ tests/client_python_verify.erl
@@ -21,7 +21,7 @@ confirm() ->
     lager:info("Enabling search hook on 'searchbucket'"),
     rt:enable_search_hook(Node, <<"searchbucket">>),
 
-    {ExitCode, PythonLog} = rt:stream_cmd("python setup.py develop test",
+    {ExitCode, PythonLog} = rt:stream_cmd("bin/python setup.py develop test",
                                           [{cd, ?PYTHON_CHECKOUT},
                                            {env,[{"RIAK_TEST_PB_HOST", PB_Host},
                                                  {"RIAK_TEST_PB_PORT", integer_to_list(PB_Port)},
@@ -44,6 +44,9 @@ prereqs() ->
     %% Need setuptools too
     ?assertCmd("python -c 'import setuptools'"),
 
+    %% Virtualenv will isolate this so we don't have permissions issues.
+    ?assertCmd("virtualenv --help"),
+
     %% Checkout the project and a specific tag.
     case file:read_file_info(?PYTHON_CHECKOUT) of
         {error, _} ->
@@ -54,4 +57,7 @@ prereqs() ->
     end,
     TagCmd = io_lib:format("git reset --hard ~s", [?PYTHON_CLIENT_TAG]),
     rt:stream_cmd(TagCmd, [{cd, ?PYTHON_CHECKOUT}]),
+
+    lager:info("Installing an isolated environment with virtualenv in ~s", [?PYTHON_CHECKOUT]),
+    rt:stream_cmd("virtualenv --clear --no-site-packages .", [{cd, ?PYTHON_CHECKOUT}]),
     ok.
