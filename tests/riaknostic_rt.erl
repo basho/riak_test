9c9749fb9a12bb3ca6c767c4066f653928fecd61
Jared Morrow <jared@basho.com>
2012-12-03 14:13:55 -0700

    Fix riaknostic test for Riak that includes riaknostic
    In Riak 1.3.0 riaknostic was included by default.  These
    tests assumed riaknostic was *not* installed and would
    fail on 1.3+ nodes.  This change fixes that and refactors
    the test to be a little more readable.

--- tests/riaknostic_rt.erl
+++ tests/riaknostic_rt.erl
@@ -24,66 +24,93 @@
 %% Change when a new release comes out.
 -define(RIAKNOSTIC_URL, "https://github.com/basho/riaknostic/downloads/riaknostic-1.0.2.tar.gz").
 
+%% REQUIRES (sh, curl, tar)
+
 confirm() ->
     %% Build a small cluster
     [Node1, _Node2] = rt:build_cluster(2, []),
+    ?assertEqual(ok, rt:wait_until_nodes_ready([Node1])),
 
-    %% Since we don't have command output matching yet, we'll just
-    %% print it to the console and hope for the best.
-    lager:info("1. Check download message"),
-    {ok, RiaknosticOut1} = rt:admin(Node1, ["diag"]),
-    ?assert(rt:str(RiaknosticOut1, "is not present!")),
-    %%dut.cmd_output_match('Riak diagnostics utility is not present')
+    %% Install riaknostic for Riak versions below 1.3.0
+    riaknostic_bootstrap(Node1),
 
-    %% Install
-    lager:info("2a. Install Riaknostic"),
-    {ok, LibDir} = rpc:call(Node1, application, get_env, [riak_core, platform_lib_dir]),
-    Cmd = io_lib:format("sh -c \"cd ~s && curl -O -L ~s && tar xzf ~s\"", [LibDir, ?RIAKNOSTIC_URL, filename:basename(?RIAKNOSTIC_URL)]),
-    lager:info("Running command: ~s", [Cmd]),
-    lager:debug("~p~n", [rpc:call(Node1, os, cmd, [Cmd])]),
-    
-    %% Execute
-    lager:info("2b. Check Riaknostic executes"),
-    {ok, RiaknosticOut2b} = rt:admin(Node1, ["diag"]),
-    ?assertNot(rt:str(RiaknosticOut2b, "is not present!")),
-    ?assertNot(rt:str(RiaknosticOut2b, "[debug]")),
-    
-    %% Check usage message
-    lager:info("3. Run Riaknostic usage message"),
-    {ok, RiaknosticOut3} = rt:admin(Node1, ["diag", "--help"]),
-    ?assert(rt:str(RiaknosticOut3, "Usage: riak-admin")),
-    
-    %% Check commands list
-    lager:info("4. Run Riaknostic commands list message"),
-    {ok, RiaknosticOut4} = rt:admin(Node1, ["diag", "--list"]),
-    ?assert(rt:str(RiaknosticOut4, "Available diagnostic checks")),
-    ?assert(rt:str(RiaknosticOut4, "  disk           ")),
-    ?assert(rt:str(RiaknosticOut4, "  dumps          ")),
-    ?assert(rt:str(RiaknosticOut4, "  memory_use     ")),
-    ?assert(rt:str(RiaknosticOut4, "  nodes_connected")),
-    ?assert(rt:str(RiaknosticOut4, "  ring_membership")),
-    ?assert(rt:str(RiaknosticOut4, "  ring_preflists ")),
-    ?assert(rt:str(RiaknosticOut4, "  ring_size      ")),
-    ?assert(rt:str(RiaknosticOut4, "  search         ")),
-    
-    %% Check log levels
-    lager:info("5. Run Riaknostic with a different log level"),
-    {ok, RiaknosticOut5} = rt:admin(Node1, ["diag", "--level", "debug"]),
-    ?assert(rt:str(RiaknosticOut5, "[debug]")),
+    %% Run through all tests on Node1
+    check_riaknostic_exectute(Node1),
+    check_riaknostic_usage(Node1),
+    check_riaknostic_command_list(Node1),
+    check_riaknostic_log_levels(Node1),
+    check_riaknostic_conn_failure(Node1),
 
-    %% Check node conn failure when stopped
-    lager:info("6. Riaknostic warns of node connection failure when stopped"),
-    rt:stop(Node1),
-    {ok, RiaknosticOut6} = rt:admin(Node1, ["diag"]),
-    ?assert(rt:str(RiaknosticOut6, "[warning] Could not connect")),
-    
-    %% Check node connection when started
-    lager:info("7. Riaknostic connects to node when running"),
-    rt:start(Node1),
-    {ok, RiaknosticOut7} = rt:admin(Node1, ["diag", "--level", "debug"]),
-    ?assert(rt:str(RiaknosticOut7, "[debug] Not connected")),
-    ?assert(rt:str(RiaknosticOut7, "[debug] Connected to local Riak node")),
-    
     %% Done!
     lager:info("Test riaknostic: PASS"),
     pass.
+
+riaknostic_bootstrap(Node) ->
+    lager:info("Check if riaknostic is installed"),
+    {ok, RiaknosticOut1} = rt:admin(Node, ["diag"]),
+    riaknostic_install((rt:str(RiaknosticOut1, "is not present!")), Node).
+
+%% riaknostic is already installed, move along
+riaknostic_install(false, _Node) ->
+    ok;
+
+%% install riaknostic
+riaknostic_install(true, Node) ->
+    %% Install
+    lager:info("Installing Riaknostic"),
+    {ok, LibDir} = rpc:call(Node, application, get_env, [riak_core, platform_lib_dir]),
+    Cmd = io_lib:format("sh -c \"cd ~s && curl -O -L ~s && tar xzf ~s\"",
+                        [LibDir, ?RIAKNOSTIC_URL, filename:basename(?RIAKNOSTIC_URL)]),
+    lager:info("Running command: ~s", [Cmd]),
+    lager:debug("~p~n", [rpc:call(Node, os, cmd, [Cmd])]),
+    ok.
+
+%% Check that riaknostic executes
+check_riaknostic_exectute(Node) ->
+    %% Execute
+    lager:info("**  Check Riaknostic executes"),
+    {ok, RiaknosticOut} = rt:admin(Node, ["diag"]),
+    ?assertNot(rt:str(RiaknosticOut, "is not present!")),
+    ?assertNot(rt:str(RiaknosticOut, "[debug]")),
+    ok.
+
+%% Check that riaknostic gives a usage message
+check_riaknostic_usage(Node) ->
+    %% Check usage message
+    lager:info("**  Run Riaknostic usage message"),
+    {ok, RiaknosticOut} = rt:admin(Node, ["diag", "--help"]),
+    ?assert(rt:str(RiaknosticOut, "Usage: riak-admin")),
+    ok.
+
+%% Check that riaknostic gives a command listing
+check_riaknostic_command_list(Node) ->
+    %% Check commands list
+    lager:info("**  Run Riaknostic commands list message"),
+    {ok, RiaknosticOut} = rt:admin(Node, ["diag", "--list"]),
+    ?assert(rt:str(RiaknosticOut, "Available diagnostic checks")),
+    ?assert(rt:str(RiaknosticOut, "  disk           ")),
+    ?assert(rt:str(RiaknosticOut, "  dumps          ")),
+    ?assert(rt:str(RiaknosticOut, "  memory_use     ")),
+    ?assert(rt:str(RiaknosticOut, "  nodes_connected")),
+    ?assert(rt:str(RiaknosticOut, "  ring_membership")),
+    ?assert(rt:str(RiaknosticOut, "  ring_preflists ")),
+    ?assert(rt:str(RiaknosticOut, "  ring_size      ")),
+    ?assert(rt:str(RiaknosticOut, "  search         ")),
+    ok.
+
+%% Check that log levels can be set
+check_riaknostic_log_levels(Node) ->
+    %% Check log levels
+    lager:info("**  Run Riaknostic with a different log level"),
+    {ok, RiaknosticOut} = rt:admin(Node, ["diag", "--level", "debug"]),
+    ?assert(rt:str(RiaknosticOut, "[debug]")),
+    ok.
+
+%% Check that a connection failure message is output if node is stopped
+check_riaknostic_conn_failure(Node) ->
+    %% Check node conn failure when stopped
+    lager:info("**  Riaknostic warns of node connection failure when stopped"),
+    rt:stop_and_wait(Node),
+    {ok, RiaknosticOut} = rt:admin(Node, ["diag"]),
+    ?assert(rt:str(RiaknosticOut, "[warning] Could not connect")),
+    ok.
