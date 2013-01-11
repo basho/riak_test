23326603538fe780c0b50170d98a1d47c217cd86
Jared Morrow <jared@basho.com>
2013-01-11 12:56:25 -0700

    Make intercepts wait on the node-up before rpc:call's
    Intercepts get called whether they are used or not in every test.
    In all cases of calling 'riak start' intercepts are loaded
    before waiting on the node to be confirmed up which causes
    rpc failures.

--- src/rt_intercept.erl
+++ src/rt_intercept.erl
@@ -37,6 +37,8 @@ add(Node, {Target, Intercept, Mapping}) ->
     ok = rpc:call(Node, intercept, add, [Target, Intercept, Mapping]).
 
 remote_compile_and_load(Node, F) ->
+    lager:info("Waiting for node ~s to come up before loading code", [Node]),
+    rt:wait_until_pingable(Node),
     lager:info("Compiling and loading file ~s on node ~s", [F, Node]),
     {ok, _, Bin} = rpc:call(Node, compile, file, [F, [binary]]),
     ModName = list_to_atom(filename:basename(F, ".erl")),
