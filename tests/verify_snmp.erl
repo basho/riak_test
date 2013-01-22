0c4dd17886d9aa929e2ac627b1fc3a2a0a01c805
Sean Cribbs <sean@basho.com>
2013-01-22 12:51:41 -0600

    Use wait_until instead of random sleeps to compare stats.

--- tests/verify_snmp.erl
+++ tests/verify_snmp.erl
@@ -23,19 +23,18 @@
 -export([confirm/0]).
 -include_lib("eunit/include/eunit.hrl").
 
+-prereq("curl").
+
 confirm() ->
     %% Bring up a small cluster
     [Node1] = rt:deploy_nodes(1),
     ?assertEqual(ok, rt:wait_until_nodes_ready([Node1])),
 
-    lager:info("perform 5 x  PUT and a GET to increment the stats"),
-    lager:info("as the stat system only does calcs for > 5 readings"),
+    lager:info("Doing some reads and writes to record some stats."),
+    
+    rt:systest_write(Node1, 10),
+    rt:systest_read(Node1, 10),
 
-    %% TODO: Add cats/pugs to test other stats
-    C = rt:httpc(Node1),
-    [rt:httpc_write(C, <<"systest">>, <<X>>, <<"12345">>) || X <- lists:seq(1, 5)],
-    [rt:httpc_read(C, <<"systest">>, <<X>>) || X <- lists:seq(1, 5)],
-    timer:sleep(10000),
     Stats = get_stats(Node1),
     Keys = [{vnodeGets,<<"vnode_gets">>},
             {vnodePuts,<<"vnode_puts">>},
@@ -51,23 +50,31 @@ confirm() ->
             {nodePutTime95,<<"node_put_fsm_time_95">>},
             {nodePutTime99,<<"node_put_fsm_time_99">>},
             {nodePutTime100,<<"node_put_fsm_time_100">>}],
-    verify_eq(Stats, Keys),
+    verify_eq(Stats, Keys, Node1),
     pass.
 
-verify_eq(Stats, Keys) ->
-    [begin
-         Http = proplists:get_value(StatKey, Stats),
-         Snmp = get_snmp(SnmpKey),
-         lager:info("Name: ~p HTTP: ~p SNMP: ~p", [SnmpKey, Http, Snmp]),
-         ?assertEqual(Http, Snmp)
-     end || {SnmpKey, StatKey} <- Keys].
+verify_eq(Stats, Keys, Node) ->
+    [ begin
+          Http = proplists:get_value(StatKey, Stats),
+          lager:info("Waiting until ~p matches HTTP value: ~p", [SnmpKey, Http]),
+          ?assertEqual(ok, wait_until_stat_matches(Node, SnmpKey, Http))
+      end
+      || {SnmpKey, StatKey} <- Keys].
 
 get_stats(Node) ->
     StatString = os:cmd(io_lib:format("curl -s -S ~s/stats", [rt:http_url(Node)])),
     {struct, Stats} = mochijson2:decode(StatString),
     Stats.
 
-get_snmp(StatName) ->
-    {value, OID} = snmpa:name_to_oid(StatName),
-    [Stat] = snmpa:get(snmp_master_agent, [OID ++ [0]]),
+get_snmp(Node, StatName) ->
+    {value, OID} = rpc:call(Node, snmpa, name_to_oid, [StatName]),
+    [Stat] = rpc:call(Node, snmpa, get, [snmp_master_agent, [OID ++ [0]]]),
     Stat.
+
+wait_until_stat_matches(Node, SnmpKey, Http) ->
+    rt:wait_until(Node,
+                  fun(N) ->
+                          Snmp = get_snmp(N, SnmpKey),
+                          lager:debug("SNMP: ~p is ~p", [SnmpKey, Snmp]),
+                          Http == Snmp
+                  end).
