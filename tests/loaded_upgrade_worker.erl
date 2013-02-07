f331bd196a9ed0397ef51429b9e91e794233bd7b
Joe DeVivo <joe.devivo@basho.com>
2013-02-07 11:10:46 -0700

    loaded_upgrade_worker gen_server to supervisor

--- tests/loaded_upgrade_worker.erl
+++ tests/loaded_upgrade_worker.erl
@@ -22,164 +22,57 @@
 -include_lib("eunit/include/eunit.hrl").
 -include_lib("riakc/include/riakc.hrl").
 
--behaviour(gen_server).
+-behavior(supervisor).
 
 %% API
--export([start_link/3, assert_equal/3]).
+-export([assert_equal/2]).
 
--export([list_keys_tester/4, kv_tester/4, mapred_tester/4, 
-         twoi_tester/4, search_tester/4]).
+-export([list_keys_tester/3, kv_tester/3, mapred_tester/3, 
+         twoi_tester/3, search_tester/3, start_link/2]).
 
-%% gen_server callbacks
--export([init/1, handle_call/3, handle_cast/2, handle_info/2,
-         terminate/2, code_change/3]).
+-export([init/1]).
+-export([start_link/3]).
 
--record(state, {
-          name,
-          node=undefined,
-          list_keys,
-          mapred,
-          kv,
-          twoi,
-          search,
-          state=active
-        }).
+%% Helper macro for declaring children of supervisor
+-define(CHILD(Name, FunName, Node), { 
+    list_to_atom(atom_to_list(Name) ++ "_" ++ atom_to_list(FunName)),
+    {   ?MODULE, 
+        start_link, 
+        [FunName, Node]}, 
+        permanent, 5000, worker, [?MODULE]}).
 
-%%%===================================================================
-%%% API
-%%%===================================================================
-
-%%--------------------------------------------------------------------
-%% @doc
-%% Starts the server
-%%
-%% @spec start_link() -> {ok, Pid} | ignore | {error, Error}
-%% @end
-%%--------------------------------------------------------------------
 start_link(Name, Node, Backend) ->
-    gen_server:start_link({local, Name}, ?MODULE, [Name, Node, Backend], []).
+    supervisor:start_link(?MODULE, [Name, Node, Backend]).
 
-%%%===================================================================
-%%% gen_server callbacks
-%%%===================================================================
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Initializes the server
-%%
-%% @spec init(Args) -> {ok, State} |
-%%                     {ok, State, Timeout} |
-%%                     ignore |
-%%                     {stop, Reason}
-%% @end
-%%--------------------------------------------------------------------
 init([Name, Node, Backend]) ->
     rt:wait_for_service(Node, [riak_search,riak_kv,riak_pipe]),
 
-    ListKeysPid = spawn_link(?MODULE, list_keys_tester, [Name, Node, 0, undefined]),
+    ChildSpecs1 = [ 
+        ?CHILD(Name, FunName, Node) 
+        || FunName <- [list_keys_tester, mapred_tester, kv_tester, search_tester]],
 
-    MapRedPid = spawn_link(?MODULE, mapred_tester, [Name, Node, 0, undefined]),
-    
-    KVPid = spawn_link(?MODULE, kv_tester, [Name, Node, 0, undefined]),
-    
-    TwoIPid = case Backend of
+    ChildSpecs = case Backend of
         eleveldb ->
-            spawn_link(?MODULE, twoi_tester, [Name, Node, 0, undefined]);
-        _ -> undefined
+            [?CHILD(Name, twoi_tester, Node) | ChildSpecs1];
+        _ -> ChildSpecs1
     end,
+    {ok, {{one_for_one, 1000, 60}, ChildSpecs}}.
 
-    SearchPid = spawn_link(?MODULE, search_tester, [Name, Node, 0, undefined]),
-    {ok, #state{name=Name, 
-                node=Node, 
-                list_keys=ListKeysPid, 
-                mapred=MapRedPid,
-                kv=KVPid,
-                twoi=TwoIPid,
-                search=SearchPid
-               }}.
-
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Handling call messages
-%%
-%% @spec handle_call(Request, From, State) ->
-%%                                   {reply, Reply, State} |
-%%                                   {reply, Reply, State, Timeout} |
-%%                                   {noreply, State} |
-%%                                   {noreply, State, Timeout} |
-%%                                   {stop, Reason, Reply, State} |
-%%                                   {stop, Reason, State}
-%% @end
-%%--------------------------------------------------------------------
-handle_call(_Request, _From, State) ->
-    Reply = ok,
-    {reply, Reply, State}.
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Handling cast messages
-%%
-%% @spec handle_cast(Msg, State) -> {noreply, State} |
-%%                                  {noreply, State, Timeout} |
-%%                                  {stop, Reason, State}
-%% @end
-%%--------------------------------------------------------------------
-handle_cast(_Msg, State) ->
-    {noreply, State}.
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Handling all non call/cast messages
-%%
-%% @spec handle_info(Info, State) -> {noreply, State} |
-%%                                   {noreply, State, Timeout} |
-%%                                   {stop, Reason, State}
-%% @end
-%%--------------------------------------------------------------------
-handle_info(_Info, State) ->
-    {noreply, State}.
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% This function is called by a gen_server when it is about to
-%% terminate. It should be the opposite of Module:init/1 and do any
-%% necessary cleaning up. When it returns, the gen_server terminates
-%% with Reason. The return value is ignored.
-%%
-%% @spec terminate(Reason, State) -> void()
-%% @end
-%%--------------------------------------------------------------------
-terminate(_Reason, _State) ->
-    ok.
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Convert process state when code is changed
-%%
-%% @spec code_change(OldVsn, State, Extra) -> {ok, NewState}
-%% @end
-%%--------------------------------------------------------------------
-code_change(_OldVsn, State, _Extra) ->
-    {ok, State}.
 
 %%%===================================================================
 %%% Internal functions
 %%%===================================================================
 
-list_keys_tester(Name, Node, Count, Pid) ->
+start_link(Function, Node) ->
+    {ok, spawn_link(?MODULE, Function, [Node, 0, undefined])}.
+
+list_keys_tester(Node, Count, Pid) ->
     PBC = pb_pid_recycler(Pid, Node),
     case riakc_pb_socket:list_keys(PBC, <<"objects">>) of
         {ok, Keys} ->
             ActualKeys = lists:usort(Keys),
             ExpectedKeys = lists:usort([loaded_upgrade:int_to_key(K) || K <- lists:seq(0, 100)]),
-            case assert_equal(Name, ExpectedKeys, ActualKeys) of
+            case assert_equal(ExpectedKeys, ActualKeys) of
                 true -> cool;
                 _ -> loaded_upgrade ! {listkeys, not_equal}
             end;
@@ -190,9 +83,9 @@ list_keys_tester(Name, Node, Count, Pid) ->
         Unexpected ->
             loaded_upgrade ! {listkeys, Unexpected}
     end,
-    list_keys_tester(Name, Node, Count + 1, PBC).
+    list_keys_tester(Node, Count + 1, PBC).
 
-kv_tester(Name, Node, Count, Pid) ->
+kv_tester(Node, Count, Pid) ->
     PBC = pb_pid_recycler(Pid, Node),
     Key = Count rem 8000,
     case riakc_pb_socket:get(PBC, loaded_upgrade:bucket(kv), loaded_upgrade:int_to_key(Key)) of
@@ -206,11 +99,10 @@ kv_tester(Name, Node, Count, Pid) ->
         Unexpected ->
             loaded_upgrade ! {kv, Unexpected}
     end,
-    kv_tester(Name, Node, Count + 1, PBC).
+    kv_tester(Node, Count + 1, PBC).
 
-mapred_tester(Name, Node, Count, Pid) ->
+mapred_tester(Node, Count, Pid) ->
     PBC = pb_pid_recycler(Pid, Node),
-    %%lager:debug("<~p> mapred test #~p", [Name, Count]),
     case riakc_pb_socket:mapred(PBC, loaded_upgrade:bucket(mapred), loaded_upgrade:erlang_mr()) of
         {ok, [{1, [10000]}]} ->
             ok;
@@ -246,9 +138,9 @@ mapred_tester(Name, Node, Count, Pid) ->
         Unexpected ->
             loaded_upgrade ! {mapred, Unexpected}
     end,
-    mapred_tester(Name, Node, Count + 1, PBC).
+    mapred_tester(Node, Count + 1, PBC).
 
-twoi_tester(Name, Node, Count, Pid) ->
+twoi_tester(Node, Count, Pid) ->
     PBC = pb_pid_recycler(Pid, Node),
     Key = Count rem 8000,
     ExpectedKeys = [loaded_upgrade:int_to_key(Key)],
@@ -265,7 +157,7 @@ twoi_tester(Name, Node, Count, Pid) ->
                               Key + 1)
      } of 
         {{ok, BinKeys}, {ok, IntKeys}} ->
-            case {assert_equal(Name, ExpectedKeys, BinKeys), assert_equal(Name, ExpectedKeys, IntKeys)} of
+            case {assert_equal(ExpectedKeys, BinKeys), assert_equal(ExpectedKeys, IntKeys)} of
                 {true, true} -> cool;
                 {false, false} ->
                     loaded_upgrade ! {twoi, bolth_no_match};
@@ -281,9 +173,9 @@ twoi_tester(Name, Node, Count, Pid) ->
         Unexpected ->
             loaded_upgrade ! Unexpected
     end,
-    twoi_tester(Name, Node, Count + 1, PBC).
+    twoi_tester(Node, Count + 1, PBC).
 
-search_tester(Name, Node, Count, Pid) ->
+search_tester(Node, Count, Pid) ->
     PBC = pb_pid_recycler(Pid, Node),
     {Term, Size} = search_check(Count),
     case riakc_pb_socket:search(PBC, loaded_upgrade:bucket(search), Term) of
@@ -309,17 +201,10 @@ search_tester(Name, Node, Count, Pid) ->
                 _ ->
                     loaded_upgrade ! {search, {error, badfun}}
             end;
-        %%{error, Reason} when is_binary(Reason) ->
-        %%    case string:str(binary_to_list(Reason), "badfun") of
-        %%        0 -> %% This is not badfun, probably a connection issue
-        %%            ok;
-        %%        _ -> %% this is badfun
-        %%            ?assert(rt:is_mixed_cluster(Node))
-        %%    end
         Unexpected ->
             loaded_upgrade ! {search, Unexpected}
     end,
-    search_tester(Name, Node, Count + 1, PBC).
+    search_tester(Node, Count + 1, PBC).
 
 search_check(Count) ->
     case Count rem 6 of
@@ -331,10 +216,10 @@ search_check(Count) ->
         5 -> { <<"prohibiting">>, 5}
     end.
 
-assert_equal(Name, Expected, Actual) ->
+assert_equal(Expected, Actual) ->
     case Expected -- Actual of 
         [] -> ok;
-        Diff -> lager:info("<~p> Expected -- Actual: ~p", [Name, Diff])
+        Diff -> lager:info("Expected -- Actual: ~p", [Diff])
     end,
     Actual == Expected.
 
