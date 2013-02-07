96814383342cacc17f50264c93aff924599a6a61
Joe DeVivo <joe.devivo@basho.com>
2013-02-07 09:38:06 -0700

    Everyting from PR #196 except the big ones

--- tests/loaded_upgrade.erl
+++ tests/loaded_upgrade.erl
@@ -31,7 +31,6 @@ confirm() ->
     %% Let's spawn workers against it.
     timer:sleep(10000),
 
-    %% TODO: Then, kill a specific supervisor
     Sups = [
         {rt_worker_sup:start_link([
             {concurrent, 10},
@@ -40,8 +39,6 @@ confirm() ->
         ]), Node}
     || Node <- Nodes],
 
-    %% TODO: Replace with a recieve block 
-    %%timer:sleep(?TIME_BETWEEN_UPGRADES * 100),
     upgrade_recv_loop(),
 
     [begin
@@ -61,7 +58,6 @@ confirm() ->
 
     pass.
 
-
 upgrade_recv_loop() ->
     {SMega, SSec, SMicro} = os:timestamp(),
     EndSecs = SSec + ?TIME_BETWEEN_UPGRADES,
@@ -73,6 +69,7 @@ upgrade_recv_loop() ->
     end,
     upgrade_recv_loop(EndTime).
 
+%% TODO: Collect error message counts in ets table
 upgrade_recv_loop(EndTime) ->
     Now = os:timestamp(),
     case Now > EndTime of
@@ -94,7 +91,7 @@ upgrade_recv_loop(EndTime) ->
         end
     end.
 
-seed_cluster(_Nodes=[Node1|_]) ->
+seed_cluster(Nodes=[Node1|_]) ->
     lager:info("Seeding Cluster"),
 
     %% For List Keys
@@ -102,26 +99,25 @@ seed_cluster(_Nodes=[Node1|_]) ->
     rt:systest_write(Node1, 100, 3),
     ?assertEqual([], rt:systest_read(Node1, 100, 1)),
 
-    Pid = rt:pbc(Node1),
-    Keys = [list_to_binary(["", integer_to_list(Ki)]) || Ki <- lists:seq(0, 100)],
-    Vals = [list_to_binary(["", integer_to_list(Ki)]) || Ki <- lists:seq(0, 100)],
-    [rt:pbc_write(Pid, <<"objects">>, Key, Val) || {Key, Val} <- lists:zip(Keys, Vals)],
+    seed(Node1, 0, 100, fun(Key) ->
+        %%Bin = list_to_binary(["", integer_to_list(Key)]),
+        Bin = iolist_to_binary(io_lib:format("~p", [Key])),
+        riakc_obj:new(<<"objects">>, Bin, Bin)
+    end),
 
     %% For KV
-    kv_seed(Pid),
+    kv_seed(Node1),
 
     %% for 2i
-    twoi_seed(Pid),
+    twoi_seed(Node1),
 
     %% for mapred
-    mr_seed(Pid),
+    mr_seed(Node1),
 
     %% For MC Serch
     rt:enable_search_hook(Node1, bucket(search)),
-    seed_search(Pid),
-
-
-    riakc_pb_socket:stop(Pid).
+    rt:wait_until_ring_converged(Nodes),
+    seed_search(Node1).
 
 %% Buckets
 bucket(kv) -> <<"utest">>;
@@ -129,13 +125,15 @@ bucket(twoi) -> <<"2ibuquot">>;
 bucket(mapred) -> <<"bryanitbs">>;
 bucket(search) -> <<"scotts_spam">>.
 
-seed_search(Pid) ->
+seed_search(Node) ->
+    Pid = rt:pbc(Node),
     SpamDir = rt:config(spam_dir),
     Files = case SpamDir of
             undefined -> undefined;
             _ -> filelib:wildcard(SpamDir ++ "/*")
         end,
-    seed_search(Pid, Files).
+    seed_search(Pid, Files),
+    riakc_pb_socket:stop(Pid).
 
 seed_search(_Pid, []) -> ok;
 seed_search(Pid, [File|Files]) ->
@@ -143,9 +141,11 @@ seed_search(Pid, [File|Files]) ->
     rt:pbc_put_file(Pid, bucket(search), Key, File),
     seed_search(Pid, Files).
 
-kv_seed(Pid) ->
-    %% Keys: 0 - 7999
-    [ rt:pbc_write(Pid, bucket(kv), list_to_binary(["", integer_to_list(Key)]), kv_valgen(Key)) || Key <- lists:seq(0, 7999)].
+kv_seed(Node) ->
+    ValFun = fun(Key) ->
+            riakc_obj:new(bucket(kv), list_to_binary(["", integer_to_list(Key)]), kv_valgen(Key))
+    end,
+    seed(Node, 0, 7999, ValFun).
 
 kv_valgen(Key) ->
     term_to_binary(lists:seq(0, Key)).
@@ -156,32 +156,40 @@ int_to_key(KInt) ->
 %% Every 2i seeded object will have indexes
 %% int_plusone -> [Key + 1, Key + 10000]
 %% bin_plustwo -> [<<"Key + 2">>]
-twoi_seed(Pid) ->
-    %% Keys: 0 - 7999
-    [ begin
+twoi_seed(Node) ->
+    ValFun = fun(Key) ->
         Obj = riakc_obj:new(bucket(twoi), list_to_binary(["", integer_to_list(Key)]), kv_valgen(Key)),
         MD1 = riakc_obj:get_update_metadata(Obj),
         MD2 = riakc_obj:set_secondary_index(MD1, [
             {{integer_index, "plusone"}, [Key + 1, Key + 10000]},
             {{binary_index, "plustwo"}, [int_to_key(Key + 2)]}
         ]),
-        Obj2 = riakc_obj:update_metadata(Obj, MD2),
-        riakc_pb_socket:put(Pid, Obj2)
-      end || Key <- lists:seq(0, 7999)].
+        riakc_obj:update_metadata(Obj, MD2)
+    end,
+    seed(Node, 0, 7999, ValFun).
 
 erlang_mr() ->
     [{map, {modfun, riak_kv_mapreduce, map_object_value}, none, false},
          {reduce, {modfun, riak_kv_mapreduce, reduce_count_inputs}, none, true}].
 
-mr_seed(Pid) ->
+mr_seed(Node) ->
 %% to be used along with sequential_int keygen to populate known
 %% mapreduce set
-    [ begin
-        Value = list_to_binary(["", integer_to_list(Key)]),
-        Obj = riakc_obj:new(bucket(mapred), list_to_binary(["", integer_to_list(Key)]), Value),
-        riakc_pb_socket:put(Pid, Obj)
-      end || Key <- lists:seq(0, 9999)].
+    ValFun = fun(Key) ->
+            Value = list_to_binary(["", integer_to_list(Key)]),
+            riakc_obj:new(bucket(mapred), Value, Value)
+        end,
+    seed(Node, 0, 9999, ValFun).
 
+seed(Node, Start, End, ValFun) ->
+    PBC = rt:pbc(Node),
+
+    [ begin
+        Obj = ValFun(Key),
+        riakc_pb_socket:put(PBC, Obj)
+    end || Key <- lists:seq(Start, End)],
+
+    riakc_pb_socket:stop(PBC).
 
 %% ===================================================================
 %% Monitor nodes after they upgrade
