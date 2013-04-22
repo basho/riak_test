6fb396562eb92d05265d5c064ab6dcbc842df933
Joe DeVivo <joe.devivo@basho.com>
2013-04-22 15:01:00 -0700

    test and riak version specific purge I

--- intercepts/riak_core_ring_manager_intercepts.erl
+++
@@ -1,7 +0,0 @@
--module(riak_core_ring_manager_intercepts).
--compile(export_all).
--include("intercept.hrl").
-
-noop_ring_trans(_Fun, _Args) ->
-    ?I_INFO("overriding ring_trans as a noop"),
-    not_changed.
