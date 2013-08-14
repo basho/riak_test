6db3f29247072c853764ddf7609ed158c822175c
Jon Anderson <janderson@basho.com>
2013-08-14 17:22:45 -0400

    Riak Test to simulate overload condition where riak_repl2_rtq rmailbox fills, for testing traffic shedding fix

---
+++ intercepts/riak_repl2_rtq_intercepts.erl
@@ -0,0 +1,13 @@
+%% Intercepts functions for the riak_test in ../tests/repl_rt_heartbeat.erl
+-module(riak_repl2_rtq_intercepts).
+-compile(export_all).
+-include("intercept.hrl").
+
+-define(M, riak_repl2_rtq).
+
+%% @doc Drop the heartbeat messages from the rt source.
+slow_trim_q(State) ->
+    %% ?I_INFO("slow_"),
+    timer:sleep(5000),
+    ?M:trim_q(State),
+    ok.
