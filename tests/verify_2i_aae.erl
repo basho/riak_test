efabce2abc2afe42c4d7b6600fb518b6df6f9207
Joseph Blomstedt <joe@basho.com>
2013-12-11 16:57:33 -0800

    Extend verify_2i_aae test

--- tests/verify_2i_aae.erl
+++ tests/verify_2i_aae.erl
@@ -45,33 +45,89 @@ confirm() ->
                        {{diff_index_specs, 2}, skippable_diff_index_specs}]}),
     lager:info("Installed intercepts to corrupt index specs on node ~p", [Node1]),
     PBC = rt:pbc(Node1),
-    Index = {integer_index, "i"},
-    set_skip_index_specs(Node1, false),
     NumItems = ?NUM_ITEMS,
     %%NumDelItems = NumItems div 10,
+    NumDel = ?NUM_DELETES,
+    pass = check_lost_objects(Node1, PBC, NumItems, NumDel),
+    pass = check_lost_indexes(Node1, PBC, NumItems),
+    pass = check_kill_repair(Node1),
+    lager:info("Et voila"),
+    riakc_pb_socket:stop(PBC),
+    pass.
+
+%% Write objects with a 2i index. Modify/delete the objects without updating
+%% the 2i index. Test that running 2i repair corrects the 2i indexes.
+check_lost_objects(Node1, PBC, NumItems, NumDel) ->
+    Index = {integer_index, "i"},
+    set_skip_index_specs(Node1, false),
     lager:info("Putting ~p objects with indexes", [NumItems]),
-    [[put_obj(PBC, Bucket, N, N+1) || N <- lists:seq(1, NumItems)]
-     || Bucket <- ?BUCKETS],
-    % Verify they are there.
+    [put_obj(PBC, Bucket, N, N+1, Index) || N <- lists:seq(1, NumItems),
+                                            Bucket <- ?BUCKETS],
+    %% Verify they are there.
     ExpectedInitial = [{to_key(N+1), to_key(N)} || N <- lists:seq(1, NumItems)],
     lager:info("Check objects are there as expected"),
     [assert_range_query(PBC, Bucket, ExpectedInitial, Index, 1, NumItems+1)
      || Bucket <- ?BUCKETS],
     lager:info("Now mess index spec code and change values"),
     set_skip_index_specs(Node1, true),
-    NumDel = ?NUM_DELETES,
-    [[put_obj(PBC, Bucket, N, N) || N <- lists:seq(1, NumItems-NumDel)]
-     || Bucket <- ?BUCKETS],
+    [put_obj(PBC, Bucket, N, N, Index) || N <- lists:seq(1, NumItems-NumDel),
+                                          Bucket <- ?BUCKETS],
     DelRange = lists:seq(NumItems-NumDel+1, NumItems),
-    [[del_obj(PBC, Bucket, N) || N <- DelRange] || Bucket <- ?BUCKETS],
+    lager:info("Deleting ~b objects without updating indexes", [NumDel]),
+    [del_obj(PBC, Bucket, N) || N <- DelRange, Bucket <- ?BUCKETS],
     DelKeys = [to_key(N) || N <- DelRange], 
     [rt:wait_until(fun() -> rt:pbc_really_deleted(PBC, Bucket, DelKeys) end)
      || Bucket <- ?BUCKETS],
-    % Verify they are damaged
+    %% Verify they are damaged
     lager:info("Verify change did not take, needs repair"),
     [assert_range_query(PBC, Bucket, ExpectedInitial, Index, 1, NumItems+1)
      || Bucket <- ?BUCKETS],
     set_skip_index_specs(Node1, false),
+    run_2i_repair(Node1),
+    lager:info("Now verify that previous changes are visible after repair"),
+    ExpectedFinal = [{to_key(N), to_key(N)} || N <- lists:seq(1, NumItems-NumDel)],
+    [assert_range_query(PBC, Bucket, ExpectedFinal, Index, 1, NumItems+1)
+     || Bucket <- ?BUCKETS],
+    pass.
+
+%% Write objects without a 2i index. Test that running 2i repair will generate
+%% the missing indexes.
+check_lost_indexes(Node1, PBC, NumItems) ->
+    set_skip_index_specs(Node1, true),
+    Index = {integer_index, "ii"},
+    lager:info("Writing ~b objects without index", [NumItems]),
+    [put_obj(PBC, Bucket, N, N+1, Index) || Bucket <- ?BUCKETS,
+                                            N <- lists:seq(1, NumItems)],
+    lager:info("Verify that objects cannot be found via index"),
+    [assert_range_query(PBC, Bucket, [], Index, 1, NumItems+1)
+     || Bucket <- ?BUCKETS],
+    run_2i_repair(Node1),
+    lager:info("Check that objects can now be found via index"),
+    Expected = [{to_key(N+1), to_key(N)} || N <- lists:seq(1, NumItems)],
+    [assert_range_query(PBC, Bucket, Expected, Index, 1, NumItems+1)
+     || Bucket <- ?BUCKETS],
+    pass.
+
+check_kill_repair(Node1) ->
+    lager:info("Test that killing 2i repair works as desired"),
+    spawn(fun() ->
+                  timer:sleep(1500),
+                  rt:admin(Node1, ["repair-2i", "kill"])
+          end),
+    ExitStatus = run_2i_repair(Node1),
+    case ExitStatus of
+        normal ->
+            lager:info("Shucks. Repair finished before we could kill it");
+        kill ->
+            lager:info("Repair was forcibly killed");
+        user_request ->
+            lager:info("Repair exited gracefully, we should be able to "
+                       "trigger another repair immediately"),
+            normal = run_2i_repair(Node1)
+    end,
+    pass.
+
+run_2i_repair(Node1) ->
     lager:info("Run 2i AAE repair"),
     ?assertMatch({ok, _}, rt:admin(Node1, ["repair-2i"])),
     RepairPid = rpc:call(Node1, erlang, whereis, [riak_kv_2i_aae]),
@@ -79,20 +135,14 @@ confirm() ->
     Mon = monitor(process, RepairPid),
     MaxWaitTime = rt_config:get(rt_max_wait_time),
     receive
-        {'DOWN', Mon, _, _, _} ->
-            ok
+        {'DOWN', Mon, _, _, Status} ->
+            lager:info("Status: ~p", [Status]),
+            Status
     after
         MaxWaitTime ->
             lager:error("Timed out (~pms) waiting for 2i AAE repair process", [MaxWaitTime]),
             ?assertEqual(aae_2i_repair_complete, aae_2i_repair_timeout)
-    end,
-    lager:info("Now verify that previous changes are visible after repair"),
-    ExpectedFinal = [{to_key(N), to_key(N)} || N <- lists:seq(1, NumItems-NumDel)],
-    [assert_range_query(PBC, Bucket, ExpectedFinal, Index, 1, NumItems+1)
-     || Bucket <- ?BUCKETS],
-    lager:info("Et voila"),
-    riakc_pb_socket:stop(PBC),
-    pass.
+    end.
 
 set_skip_index_specs(Node, Val) ->
     ok = rpc:call(Node, application, set_env,
@@ -101,7 +151,7 @@ set_skip_index_specs(Node, Val) ->
 to_key(N) ->
     list_to_binary(integer_to_list(N)).
 
-put_obj(PBC, Bucket, N, IN) ->
+put_obj(PBC, Bucket, N, IN, Index) ->
     K = to_key(N),
     Obj =
     case riakc_pb_socket:get(PBC, Bucket, K) of
@@ -111,7 +161,7 @@ put_obj(PBC, Bucket, N, IN) ->
             riakc_obj:new(Bucket, K, K)
     end,
     MD = riakc_obj:get_metadata(Obj),
-    MD2 = riakc_obj:set_secondary_index(MD, {{integer_index, "i"}, [IN]}),
+    MD2 = riakc_obj:set_secondary_index(MD, {Index, [IN]}),
     Obj2 = riakc_obj:update_metadata(Obj, MD2),
     riakc_pb_socket:put(PBC, Obj2, [{dw, ?N_VAL}]).
 
@@ -128,6 +178,12 @@ del_obj(PBC, Bucket, N) ->
 assert_range_query(Pid, Bucket, Expected0, Index, StartValue, EndValue) ->
     lager:info("Searching Index ~p/~p for ~p-~p", [Bucket, Index, StartValue, EndValue]),
     {ok, ?INDEX_RESULTS{terms=Keys}} = riakc_pb_socket:get_index_range(Pid, Bucket, Index, StartValue, EndValue, [{return_terms, true}]),
-    Actual = lists:sort(Keys),
+    Actual = case Keys of
+                 undefined ->
+                     [];
+                 _ ->
+                     lists:sort(Keys)
+             end,
     Expected = lists:sort(Expected0),
-    ?assertEqual({Bucket, Expected}, {Bucket, Actual}).
+    ?assertEqual({Bucket, Expected}, {Bucket, Actual}),
+    lager:info("Yay! ~b (actual) == ~b (expected)", [length(Actual), length(Expected)]).
