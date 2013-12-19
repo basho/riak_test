053adf96ff3f796f820c6f5762f060f687b8b312
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2013-12-19 00:32:06 -0500

    Add additional intercepts for hashtree related funs.

--- intercepts/riak_kv_index_hashtree_intercepts.erl
+++ intercepts/riak_kv_index_hashtree_intercepts.erl
@@ -4,5 +4,13 @@
 
 -define(M, riak_kv_index_hashtree_orig).
 
+%% @doc When attempting to get the lock on a hashtree, return the
+%%      not_built atom which means the tree has not been computed yet.
 not_built(_TreePid, _Type) ->
     not_built.
+
+%% @doc When attempting to get the lock on a hashtree, return the
+%%      already_locked atom which means the tree is locked by another
+%%      process.
+already_locked(_TreePid, _Type) ->
+    already_locked.
