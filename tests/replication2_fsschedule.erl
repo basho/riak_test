f863c75cd2ec6c07ed48de6d7d86df09cfdcf8e3
Dave Parfitt <dparfitt@basho.com>
2014-01-06 15:26:20 -0500

    simplify repl scheduled fs test

--- tests/replication2_fsschedule.erl
+++ tests/replication2_fsschedule.erl
@@ -3,20 +3,22 @@
 -include_lib("eunit/include/eunit.hrl").
 
 -import(rt, [deploy_nodes/2,
-join/2,
+             join/2,
              wait_until_nodes_ready/1,
              wait_until_no_pending_changes/1]).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% This tests fullsync scheduling in 1.2 and 1.3 Advanced Replication
+%% This tests fullsync scheduling in 1.4+ Advanced Replication%% intercept
+%% gets called w/ v3 test too, let it
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-setup_repl_clusters(Conf) ->
+setup_repl_clusters(Conf, InterceptSetup) ->
     NumNodes = 6,
     lager:info("Deploy ~p nodes", [NumNodes]),
 
     Nodes = deploy_nodes(NumNodes, Conf),
+    InterceptSetup(Nodes),
 
     lager:info("Nodes = ~p", [Nodes]),
     {[AFirst|_] = ANodes, Rest} = lists:split(2, Nodes),
@@ -74,7 +76,6 @@ setup_repl_clusters(Conf) ->
     ?assertEqual(ok, repl_util:wait_for_connection(LeaderA, "C")),
     rt:wait_until_ring_converged(ANodes),
 
-    %% write some data on A
     ?assertEqual(ok, repl_util:wait_for_connection(LeaderA, "B")),
 
     ?assertEqual(ok, repl_util:wait_for_connection(LeaderA, "B")),
@@ -85,10 +86,8 @@ setup_repl_clusters(Conf) ->
 
 
 test_multiple_schedules() ->
-    TestHash = erlang:md5(term_to_binary(os:timestamp())),
-    TestBucket = <<TestHash/binary, "-systest_a">>,
-
     Conf = [
+            {riak_core, [{ring_creation_size, 4}]},
             {riak_repl,
              [
               {fullsync_on_connect, false},
@@ -96,107 +95,49 @@ test_multiple_schedules() ->
              ]}
            ],
     {LeaderA, _ANodes, _BNodes, _CNodes, AllNodes} =
-        setup_repl_clusters(Conf),
-    rt:log_to_nodes(AllNodes, "Test multiple fullsync schedules from A -> [B,C]"),
-
-    lager:info("Writing 500 keys to ~p", [LeaderA]),
-    ?assertEqual([], repl_util:do_write(LeaderA, 0, 500, TestBucket, 1)),
-
-    Status0 = rpc:call(LeaderA, riak_repl_console, status, [quiet]),
-    Count = proplists:get_value(server_fullsyncs, Status0),
-    ?assertEqual(0, Count),
-
-    Start = riak_core_util:moment(),
-    lager:info("Note: Waiting for fullsyncs can take several minutes"),
-    wait_until_n_bnw_fullsyncs(LeaderA, "B", 3),
-    Finish = riak_core_util:moment(),
-    Diff = Finish - Start,
-    Minutes = Diff / 60,
-    %% Why 5? 1 minute for repl to B to start, 3 fullsyncs + room for slow boxes
-    ?assert(Minutes =< 5),
-
-    {_AFirst, BFirst, CFirst} = get_firsts(AllNodes),
-    %% verify data is replicated to B
-    lager:info("Reading 500 keys written to ~p from ~p", [LeaderA, BFirst]),
-    ?assertEqual(0, repl_util:wait_for_reads(BFirst, 0, 500, TestBucket, 2)),
-    %% verify data is replicated to C
-    lager:info("Reading 500 keys written to ~p from ~p", [LeaderA, CFirst]),
-    ?assertEqual(0, repl_util:wait_for_reads(CFirst, 0, 500, TestBucket, 2)),
-
-    FSCountToC = get_cluster_fullsyncs(LeaderA, "C"),
-    %% Why 2? 1 minute for repl to C to start, 1 fullsync
-    ?assert(FSCountToC =< 2),
+        setup_repl_clusters(Conf, fun install_v3_intercepts/1),
+    lager:info("Waiting for fullsyncs"),
+    wait_until_fullsyncs(LeaderA, "B", 5),
+    wait_until_fullsyncs(LeaderA, "C", 5),
     rt:clean_cluster(AllNodes),
     pass.
 
 test_single_schedule() ->
-    TestHash = erlang:md5(term_to_binary(os:timestamp())),
-    TestBucket = <<TestHash/binary, "-systest_a">>,
-
     Conf = [
+            {riak_core, [{ring_creation_size, 4}]},
             {riak_repl,
              [
               {fullsync_on_connect, false},
-              {fullsync_interval, 1}
+              {fullsync_interval, 99}
              ]}
            ],
     {LeaderA, _ANodes, _BNodes, _CNodes, AllNodes} =
-        setup_repl_clusters(Conf),
-    rt:log_to_nodes(AllNodes, "Test single fullsync schedule from A -> [B,C]"),
-
-    lager:info("Writing 500 keys to ~p", [LeaderA]),
-    ?assertEqual([], repl_util:do_write(LeaderA, 0, 500, TestBucket, 1)),
-
-    Status0 = rpc:call(LeaderA, riak_repl_console, status, [quiet]),
-    Count = proplists:get_value(server_fullsyncs, Status0),
-    ?assertEqual(0, Count),
-
-    Start = riak_core_util:moment(),
-    lager:info("Note: Waiting for fullsyncs can take several minutes"),
-    wait_until_n_bnw_fullsyncs(LeaderA, "B", 3),
-    Finish = riak_core_util:moment(),
-    Diff = Finish - Start,
-    Minutes = Diff / 60,
-    ?assert(Minutes =< 5 andalso Minutes >= 3),
-
-    {_AFirst, BFirst, CFirst} = get_firsts(AllNodes),
-    %% verify data is replicated to B
-    lager:info("Reading 500 keys written to ~p from ~p", [LeaderA, BFirst]),
-    ?assertEqual(0, repl_util:wait_for_reads(BFirst, 0, 500, TestBucket, 2)),
-
-    %% verify data is replicated to C
-    lager:info("Reading 500 keys written to ~p from ~p", [LeaderA, CFirst]),
-    ?assertEqual(0, repl_util:wait_for_reads(CFirst, 0, 500, TestBucket, 2)),
-
-    FSCountToC = get_cluster_fullsyncs(LeaderA, "C"),
-    %% Why 2? 1 minute for repl to C to start, 1 fullsync
-    ?assert(FSCountToC =< 5 andalso FSCountToC >= 3),
+        setup_repl_clusters(Conf, fun install_v3_intercepts/1),
+    rt:log_to_nodes(AllNodes, "Test shared fullsync schedule from A -> [B,C]"),
+    %% let some msgs queue up, doesn't matter how long we wait
+    lager:info("Waiting for fullsyncs"),
+    wait_until_fullsyncs(LeaderA, "B", 10),
+    wait_until_fullsyncs(LeaderA, "C", 10),
     rt:clean_cluster(AllNodes),
     pass.
 
-
 test_mixed_12_13() ->
-    TestHash = erlang:md5(term_to_binary(os:timestamp())),
-    TestBucket = <<TestHash/binary, "-systest_a">>,
-
     Conf = [
+            {riak_core, [{ring_creation_size, 4}]},
             {riak_repl,
              [
               {fullsync_on_connect, false},
-              {fullsync_interval, 1}
+              {fullsync_interval, 99}
              ]}
            ],
     {LeaderA, ANodes, BNodes, CNodes, AllNodes} =
-        setup_repl_clusters(Conf),
+        setup_repl_clusters(Conf, fun install_mixed_intercepts/1),
 
-    {AFirst, BFirst, _CFirst} = get_firsts(AllNodes),
+    {_AFirst, BFirst, _CFirst} = get_firsts(AllNodes),
 
     repl_util:wait_until_leader_converge(ANodes),
     repl_util:wait_until_leader_converge(BNodes),
     repl_util:wait_until_leader_converge(CNodes),
-
-    lager:info("Writing 500 keys to ~p", [LeaderA]),
-    ?assertEqual([], repl_util:do_write(LeaderA, 0, 500, TestBucket, 1)),
 
     lager:info("Adding repl listener to cluster A"),
     ListenerArgs = [[atom_to_list(LeaderA), "127.0.0.1", "9010"]],
@@ -207,37 +148,37 @@ test_mixed_12_13() ->
     SiteArgs = ["127.0.0.1", "9010", "rtmixed"],
     Res = rpc:call(BFirst, riak_repl_console, add_site, [SiteArgs]),
 
-    lager:info("Waiting until scheduled fullsync occurs. Go grab a beer, this may take awhile."),
-
-    wait_until_n_bnw_fullsyncs(LeaderA, "B", 3),
-    wait_until_n_bnw_fullsyncs(LeaderA, "C", 3),
-    %% 1.3 fullsyncs increment the 1.2 fullsync counter, backwards
-    %% compatability is a terrible thing
+    lager:info("Waiting for v2 repl to catch up. Good time to light up a cold can of Tab."),
+    wait_until_fullsyncs(LeaderA, "B", 3),
+    wait_until_fullsyncs(LeaderA, "C", 3),
     wait_until_12_fs_complete(LeaderA, 9),
-
-    Status0 = rpc:call(LeaderA, riak_repl_console, status, [quiet]),
-    Count0 = proplists:get_value(server_fullsyncs, Status0),
-    FS_B = get_cluster_fullsyncs(AFirst, "B"),
-    FS_C = get_cluster_fullsyncs(AFirst, "C"),
-    %% count the actual 1.2 fullsyncs
-    Count = Count0 - (FS_B + FS_C),
-
-    lager:info("1.2 Count = ~p", [Count]),
-    lager:info("1.3 B Count = ~p", [FS_B]),
-    lager:info("1.3 C Count = ~p", [FS_C]),
-
-    ?assert(Count >= 3 andalso Count =< 6),
-    ?assert(FS_B >= 3 andalso FS_B =< 6),
-    ?assert(FS_C >= 3 andalso FS_C =< 6),
+    rt:clean_cluster(AllNodes),
     pass.
 
 
 confirm() ->
-    AllTests = [test_multiple_schedules(), test_single_schedule(), test_mixed_12_13()],
+    AllTests = [test_mixed_12_13(), test_multiple_schedules(), test_single_schedule()],
     case lists:all(fun (Result) -> Result == pass end, AllTests) of
         true ->  pass;
         false -> sadtrombone
     end.
+
+wait_until_fullsyncs(Node, ClusterName, N) ->
+    Res = rt:wait_until(Node,
+        fun(_) ->
+                FS = get_cluster_fullsyncs(Node, ClusterName),
+                case FS of
+                    {badrpc, _} ->
+                        false;
+                    undefined ->
+                        false;
+                    X when X >= N ->
+                        true;
+                    _ ->
+                        false
+                end
+        end),
+    ?assertEqual(ok, Res).
 
 wait_until_12_fs_complete(Node, N) ->
     rt:wait_until(Node,
@@ -258,26 +199,27 @@ get_firsts(Nodes) ->
 
 get_cluster_fullsyncs(Node, ClusterName) ->
     Status = rpc:call(Node, riak_repl2_fscoordinator, status, []),
-                                                % let it fail if keys are missing
-    ClusterData = proplists:get_value(ClusterName, Status),
-    proplists:get_value(fullsyncs_completed, ClusterData).
+    case proplists:lookup(ClusterName, Status) of
+        none -> 0;
+        {_, ClusterData} ->
+            case proplists:lookup(fullsyncs_completed, ClusterData) of
+                none -> 0;
+                FSC -> FSC
+            end
+    end.
 
-wait_until_n_bnw_fullsyncs(Node, DestCluster, N) ->
-    lager:info("Waiting for fullsync count for ~p to be ~p", [DestCluster, N]),
-    Res = rt:wait_until(Node,
-        fun(_) ->
-                Fullsyncs = get_cluster_fullsyncs(Node, DestCluster),
-                case Fullsyncs of
-                    C when C >= N ->
-                        true;
-                    _Other ->
-                        %% keep this in for tracing
-                        %%lager:info("Total fullsyncs = ~p", [Other]),
-                        %% sleep a while so the default 3 minute time out
-                        %% doesn't screw us
-                        timer:sleep(20000),
-                        false
-                end
-        end),
-    ?assertEqual(ok, Res),
-    lager:info("Fullsync on ~p complete", [Node]).
+%% skip v2 repl interval checks
+install_v3_intercepts(Nodes) ->
+    [rt_intercept:add(Node, {riak_repl_util, [{{start_fullsync_timer,3},
+                                                interval_check_v3}
+                                                ]})
+               || Node <- Nodes].
+
+%% check v2 + v3 intervals
+install_mixed_intercepts(Nodes) ->
+     [rt_intercept:add(Node, {riak_repl_util, [{{start_fullsync_timer,3},
+                                                interval_check_v3},
+                                               {{schedule_fullsync,1},
+                                                interval_check_v2}]})
+             || Node <- Nodes].
+
