005a6d9fedfe41f25dce31e409529757cc6a4023
Bryce Kerley <bkerley@brycekerley.net>
2014-01-24 15:08:56 -0500

    make ruby testing more like python testing

--- tests/client_ruby_verify.erl
+++ tests/client_ruby_verify.erl
@@ -3,6 +3,11 @@
 -export([confirm/0]).
 -include_lib("eunit/include/eunit.hrl").
 
+-define(RUBY_CLIENT_TAG, "master").
+-define(RUBY_CHECKOUT, filename:join([rt_config:get(rt_scratch_dir), "riak-ruby-client"])).
+-define(RUBY_GIT_URL, "https://github.com/basho/riak-ruby-client.git").
+-define(RUBY_BUNDLE_PATH, filename:join([RUBY_CHECKOUT, "vendor/bundle"])).
+
 -prereq("ruby").
 -prereq("gem").
 
@@ -10,7 +15,8 @@
 
 confirm() ->
     prereqs(),
-    GemDir = dat_gem(),
+    clone_repo(),
+    install_dependencies(),
     Nodes = rt:deploy_nodes([{current,
                               [{riak_kv, [{test, true},
                                           {add_paths, [filename:join([GemDir, "erl_src"])]}]},
@@ -19,30 +25,13 @@ confirm() ->
                             [riak_search]),
     [Node1] = Nodes,
 
-    [{Node1, ConnectionInfo}] = rt:connection_info([Node1]),
-    {_HTTP_Host, HTTP_Port} = orddict:fetch(http, ConnectionInfo),
-    {_PB_Host, PB_Port} = orddict:fetch(pb, ConnectionInfo),
+    configure_test_client(Node1),
 
-    %% Ruby Client Tests require a path to riak, to grab the PIPE_DIR
-    %% from riak && riak-admin. Work will need to be done on the
-    %% ruby-client side to enable this test to work with remote nodes.
-    %% Fortunately for us, riak_test does not support remote nodes, so
-    %% all is fine in ruby land... for now.
-    %% That's why I'm calling rtdev directly, violating a cardinal rule
-    %% of riak_test.
-    RiakRootDir = rtdev:node_path(Node1),
-
-    Cmd = "bin/rspec --profile --tag integration --tag \~nodegen --no-color",
+    Cmd = "./bin/rspec --profile --tag integration --no-color",
 
     lager:info("Cmd: ~s", [Cmd]),
 
-    {Code, RubyLog} = rt_local:stream_cmd(Cmd, [{cd, GemDir}, {env, [
-            {"RIAK_NODE_NAME", atom_to_list(Node1)},
-            {"RIAK_ROOT_DIR", RiakRootDir},
-            {"HTTP_PORT", integer_to_list(HTTP_Port)},
-            {"PB_PORT", integer_to_list(PB_Port)},
-            {"RIAK_VERSION", binary_to_list(rt:get_version())}
-        ]}]),
+    {Code, RubyLog} = rt_local:stream_cmd(Cmd, [{cd, RUBY_CHECKOUT}]),
     ?assert(rt:str(RubyLog, " 0 failures")),
     ?assert(Code =:= 0),
     pass.
@@ -56,20 +45,31 @@ prereqs() ->
     ok.
 
 
-% Download the ruby-client gem, unpack it and build it locally
-dat_gem() ->
-    lager:info("Fetching riak-client gem"),
-    GemInstalled = os:cmd("cd " ++ rt_config:get(rt_scratch_dir) ++ " ; gem fetch riak-client"),
-    GemFile = string:substr(GemInstalled, 12, length(GemInstalled) - 12),
-    %GemFile = "riak-client",
-    lager:info("Downloaded gem: ~s", [GemFile]),
+clone_reop() ->
+    lager:info("Cleaning up ruby scratch directory"),
+    CleanupCmd = io_lib:format("rm -rf ~s", [?RUBY_CHECKOUT]),
+    os:cmd(CleanupCmd),
+    
+    lager:info("Cloning riak-ruby-client from ~s", [?RUBY_GIT_URL]),
+    CloneCmd = io_lib:format("git clone ~s ~s", [?RUBY_GIT_URL, ?RUBY_CHECKOUT]),
+    rt_local:stream_cmd(CloneCmd),
+    
+    lager:info("Resetting ruby client to tag '~s'", [?RUBY_CLIENT_TAG]),
+    TagCmd = io_lib:format("git checkout ~s", [?RUBY_CLIENT_TAG]),
+    rt_local:stream_cmd(TagCmd, [{cd, ?RUBY_CHECKOUT}]).
 
-    rt_local:stream_cmd(io_lib:format("gem unpack ~s.gem", [GemFile]), [{cd, rt_config:get(rt_scratch_dir)}]),
+install_dependencies() ->
+    lager:info("Installing ruby client dependencies"),
+    BundleCmd = "bundle install --without=guard --binstubs --no-color --path=vendor/bundle",
+    rt_local:stream_cmd(BundleCmd, [{cd, ?RUBY_CHECKOUT},
+                                    {env, [{"BUNDLE_PATH", "vendor/bundle"}]}
+                                   ]).
 
-    Cmd = "bundle install --without=guard --binstubs --no-color --path=vendor/bundle",
-    lager:info(Cmd),
-
-    GemDir = filename:join([rt_config:get(rt_scratch_dir), GemFile]),
-
-    {_Exit, _Log} = rt_local:stream_cmd(Cmd, [{cd, GemDir}, {env, [{"BUNDLE_PATH", "vendor/bundle"}]}]),
-    GemDir.
+configure_test_client(Node) ->
+    [{Node, ConnectionInfo}] = rt:connection_info([Node]),
+    Hostname = atom_to_list(Node1),
+    {_PB_Host, PB_Port} = orddict:fetch(pb, ConnectionInfo),
+    ConfigYaml = io_lib:format("host: ~s~npb_port: ~b~n", [Hostname, PB_Port]),
+    TestClientFilename = filename:join([RUBY_CHECKOUT, "spec/support/test_client.yml"]),
+    
+    ?assertMatch(ok, file:write_file(TestClientFilename, ConfigYaml)).
