bc9dd9f5be919d1804addd266d0c32dc4cd3568f
Kelly McLaughlin <kelly@basho.com>
2014-01-28 09:18:06 -0700

    Refactor repl_bucket_types test to also test fullsync

--- tests/repl_bucket_types.erl
+++ tests/repl_bucket_types.erl
@@ -19,25 +19,34 @@ confirm() ->
 
     rt:set_conf(all, [{"buckets.default.siblings", "off"}]),
     %% Start up two >1.3.2 clusters and connect them,
-    {LeaderA, LeaderB, ANodes, BNodes} = make_clusters(),
-
-    rpc:multicall([LeaderA, LeaderB], app_helper, set_env, [riak_repl, true]),
+    {LeaderA, LeaderB, ANodes, BNodes} = ClusterNodes = make_clusters(),
 
     PBA = get_pb_pid(LeaderA),
     PBB = get_pb_pid(LeaderB),
 
-    DefinedType = <<"working_type">>,
+    {DefinedType,  UndefType} = Types = {<<"working_type">>, <<"undefined_type">>},
+
     rt:create_and_activate_bucket_type(LeaderA, DefinedType, [{n_val, 3}]),
     rt:wait_until_bucket_type_status(DefinedType, active, ANodes),
 
     rt:create_and_activate_bucket_type(LeaderB, DefinedType, [{n_val, 3}]),
     rt:wait_until_bucket_type_status(DefinedType, active, BNodes),
 
-    UndefType = <<"undefined_type">>,
     rt:create_and_activate_bucket_type(LeaderA, UndefType, [{n_val, 3}]),
     rt:wait_until_bucket_type_status(UndefType, active, ANodes),
 
     connect_clusters(LeaderA, LeaderB),
+
+    realtime_test(ClusterNodes, Types, PBA, PBB),
+    fullsync_test(ClusterNodes, Types, PBA, PBB),
+
+    riakc_pb_socket:stop(PBA),
+    riakc_pb_socket:stop(PBB),
+    pass.
+
+realtime_test(ClusterNodes, BucketTypes, PBA, PBB) ->
+    {LeaderA, LeaderB, ANodes, BNodes} = ClusterNodes,
+    {DefinedType, UndefType} = BucketTypes,
 
     %% Enable RT replication from cluster "A" to cluster "B"
     lager:info("Enabling realtime between ~p and ~p", [LeaderA, LeaderB]),
@@ -49,7 +58,7 @@ confirm() ->
     DefaultObj = riakc_obj:new(Bucket, Key, Bin),
     lager:info("doing untyped put on A, bucket:~p", [Bucket]),
     riakc_pb_socket:put(PBA, DefaultObj, [{w,3}]),
-    
+
     UntypedWait = make_pbget_fun(PBB, Bucket, Key, Bin),
     ?assertEqual(ok, rt:wait_until(UntypedWait)),
 
@@ -72,31 +81,106 @@ confirm() ->
 
     lager:info("waiting for undefined type pb get on B, should get error <<\"no_type\">>"),
 
-    case riakc_pb_socket:get(PBB, UndefBucketTyped, UndefKeyTyped) of
-        {error, E} ->
-            lager:info("Got error:~p from get on cluster B", [E]),
-            ?assertEqual(<<"no_type">>, E),
-            false;
-        {ok, Res} ->
-            lager:info("Got result from get on B"),          
-            ?assertEqual(<<"data data data">>, riakc_obj:get_value(Res)),
-            false
-    end,
+    ErrorResult = riakc_pb_socket:get(PBB, UndefBucketTyped, UndefKeyTyped),
+    ?assertEqual({error, <<"no_type">>}, ErrorResult),
 
     DefaultProps = get_current_bucket_props(BNodes, DefinedType),
-    update_props(DefinedType,  [{n_val, 1}], DefaultProps, LeaderB, BNodes),
+    ?assertEqual({n_val, 3}, lists:keyfind(n_val, 1, DefaultProps)),
+
+    UpdatedProps = update_props(DefinedType,
+                                [{n_val, 1}],
+                                LeaderB,
+                                BNodes),
+    ?assertEqual({n_val, 1}, lists:keyfind(n_val, 1, UpdatedProps)),
+
+    UnequalObjBin = <<"unequal props val">>,
+    UnequalPropsObj = riakc_obj:new(BucketTyped, KeyTyped, UnequalObjBin),
+    lager:info("doing put of typed bucket on A where bucket properties "
+               "(n_val 3 versus n_val 1) are not equal on B"),
+    riakc_pb_socket:put(PBA, UnequalPropsObj, [{w,3}]),
+
+    lager:info("checking to ensure the bucket contents were not updated."),
+    ensure_bucket_not_updated(PBB, BucketTyped, KeyTyped, Bin),
+    disable_rt(LeaderA, ANodes),
+
+    UpdatedProps2 = update_props(DefinedType,
+                                 [{n_val, 3}],
+                                 LeaderB,
+                                 BNodes),
+    ?assertEqual({n_val, 3}, lists:keyfind(n_val, 1, UpdatedProps2)).
+
+fullsync_test(ClusterNodes, BucketTypes, PBA, PBB) ->
+    {LeaderA, LeaderB, ANodes, BNodes} = ClusterNodes,
+    {DefinedType, UndefType} = BucketTypes,
+
+    %% Enable RT replication from cluster "A" to cluster "B"
+    lager:info("Enabling fullsync between ~p and ~p", [LeaderA, LeaderB]),
+    enable_fullsync(LeaderA, ANodes),
+
+    Bin = <<"data data data">>,
+    Key = <<"key">>,
+    Bucket = <<"fullsync-kicked">>,
+    DefaultObj = riakc_obj:new(Bucket, Key, Bin),
+    lager:info("doing untyped put on A, bucket:~p", [Bucket]),
+    riakc_pb_socket:put(PBA, DefaultObj, [{w,3}]),
+
+    BucketTyped = {DefinedType, <<"fullsync-typekicked">>},
+    KeyTyped = <<"keytyped">>,
+    ObjTyped = riakc_obj:new(BucketTyped, KeyTyped, Bin),
+
+    lager:info("doing typed put on A, bucket:~p", [BucketTyped]),
+    riakc_pb_socket:put(PBA, ObjTyped, [{w,3}]),
+
+    UndefBucketTyped = {UndefType, <<"fullsync-badtype">>},
+    UndefKeyTyped = <<"badkeytyped">>,
+    UndefObjTyped = riakc_obj:new(UndefBucketTyped, UndefKeyTyped, Bin),
+
+    lager:info("doing typed put on A where type is not "
+               "defined on B, bucket:~p",
+               [UndefBucketTyped]),
+
+    riakc_pb_socket:put(PBA, UndefObjTyped, [{w,3}]),
+
+    {SyncTime1, _} = timer:tc(repl_util,
+                              start_and_wait_until_fullsync_complete,
+                              [LeaderA]),
+
+    lager:info("Fullsync completed in ~p seconds", [SyncTime1/1000/1000]),
+
+    ReadResult1 =  riakc_pb_socket:get(PBB, Bucket, Key),
+    ReadResult2 =  riakc_pb_socket:get(PBB, BucketTyped, KeyTyped),
+    ReadResult3 =  riakc_pb_socket:get(PBB, UndefBucketTyped, UndefKeyTyped),
+
+    ?assertMatch({ok, _}, ReadResult1),
+    ?assertMatch({ok, _}, ReadResult2),
+    ?assertMatch({error, _}, ReadResult3),
+
+    {ok, ReadObj1} = ReadResult1,
+    {ok, ReadObj2} = ReadResult2,
+
+    ?assertEqual(Bin, riakc_obj:get_value(ReadObj1)),
+    ?assertEqual(Bin, riakc_obj:get_value(ReadObj2)),
+    ?assertEqual({error, <<"no_type">>}, ReadResult3),
+
+    DefaultProps = get_current_bucket_props(BNodes, DefinedType),
+    ?assertEqual({n_val, 3}, lists:keyfind(n_val, 1, DefaultProps)),
+
+    UpdatedProps = update_props(DefinedType,  [{n_val, 1}], LeaderB, BNodes),
+    ?assertEqual({n_val, 1}, lists:keyfind(n_val, 1, UpdatedProps)),
 
     UnequalObjBin = <<"unequal props val">>,
     UnequalPropsObj = riakc_obj:new(BucketTyped, KeyTyped, UnequalObjBin),
     lager:info("doing put of typed bucket on A where bucket properties (n_val 3 versus n_val 1) are not equal on B"),
     riakc_pb_socket:put(PBA, UnequalPropsObj, [{w,3}]),
 
-    lager:info("checking to ensure the bucket contents were not updated."),
-    ensure_bucket_not_updated(PBB, BucketTyped, KeyTyped, Bin),
+    {SyncTime2, _} = timer:tc(repl_util,
+                              start_and_wait_until_fullsync_complete,
+                              [LeaderA]),
 
-    riakc_pb_socket:stop(PBA),
-    riakc_pb_socket:stop(PBB),
-    pass.
+    lager:info("Fullsync completed in ~p seconds", [SyncTime2/1000/1000]),
+
+    lager:info("checking to ensure the bucket contents were not updated."),
+    ensure_bucket_not_updated(PBB, BucketTyped, KeyTyped, Bin).
 
 %% @doc Turn on Realtime replication on the cluster lead by LeaderA.
 %%      The clusters must already have been named and connected.
@@ -105,6 +189,20 @@ enable_rt(LeaderA, ANodes) ->
     rt:wait_until_ring_converged(ANodes),
 
     repl_util:start_realtime(LeaderA, "B"),
+    rt:wait_until_ring_converged(ANodes).
+
+%% @doc Turn off Realtime replication on the cluster lead by LeaderA.
+disable_rt(LeaderA, ANodes) ->
+    repl_util:disable_realtime(LeaderA, "B"),
+    rt:wait_until_ring_converged(ANodes),
+
+    repl_util:stop_realtime(LeaderA, "B"),
+    rt:wait_until_ring_converged(ANodes).
+
+%% @doc Turn on fullsync replication on the cluster lead by LeaderA.
+%%      The clusters must already have been named and connected.
+enable_fullsync(LeaderA, ANodes) ->
+    repl_util:enable_fullsync(LeaderA, "B"),
     rt:wait_until_ring_converged(ANodes).
 
 %% @doc Connect two clusters for replication using their respective leader nodes.
@@ -128,9 +226,12 @@ make_clusters() ->
               %% turn off fullsync
               {fullsync_on_connect, false},
               {fullsync_interval, disabled},
+              {max_fssource_cluster, 20},
+              {max_fssource_node, 20},
+              {max_fssink_node, 20},
               {rtq_max_bytes, 1048576}
              ]}
-    ],
+           ],
 
     Nodes = rt:deploy_nodes(NumNodes, Conf),
     {ANodes, BNodes} = lists:split(ClusterASize, Nodes),
@@ -160,21 +261,18 @@ make_clusters() ->
     repl_util:name_cluster(BFirst, "B"),
     rt:wait_until_ring_converged(BNodes),
 
-    %% Connect for replication
-    %% connect_clusters(AFirst, BFirst),
-
     {AFirst, BFirst, ANodes, BNodes}.
 
 make_pbget_fun(Pid, Bucket, Key, Bin) ->
     fun() ->
-        case riakc_pb_socket:get(Pid, Bucket, Key) of
-            {ok, O6} ->
-                ?assertEqual(Bin, riakc_obj:get_value(O6)),
-                true;
-            _ ->
-                false
-        end
-   end.
+            case riakc_pb_socket:get(Pid, Bucket, Key) of
+                {ok, O6} ->
+                    ?assertEqual(Bin, riakc_obj:get_value(O6)),
+                    true;
+                _ ->
+                    false
+            end
+    end.
 
 ensure_bucket_not_updated(Pid, Bucket, Key, Bin) ->
     Results = [ value_unchanged(Pid, Bucket, Key, Bin) || _I <- lists:seq(1, ?ENSURE_READ_ITERATIONS)],
@@ -196,22 +294,21 @@ get_pb_pid(Leader) ->
     {ok, Pid} = riakc_pb_socket:start_link(IP, PortA, []),
     Pid.
 
-update_props(Type, Updates, DefaultProps, Node, Nodes) -> 
-    lager:info("Setting bucket properties ~p for bucket type ~p on node ~p", 
+update_props(Type, Updates, Node, Nodes) ->
+    lager:info("Setting bucket properties ~p for bucket type ~p on node ~p",
                [Updates, Type, Node]),
-    rpc:call(Node, riak_core_bucket_type, update, [Type, Updates]),    
+    rpc:call(Node, riak_core_bucket_type, update, [Type, Updates]),
     rt:wait_until_ring_converged(Nodes),
 
-    UpdatedProps = get_current_bucket_props(Nodes, Type),
-    ?assertNotEqual(DefaultProps, UpdatedProps).
-    
-%% fetch bucket properties via rpc 
+    get_current_bucket_props(Nodes, Type).
+
+%% fetch bucket properties via rpc
 %% from a node or a list of nodes (one node is chosen at random)
-get_current_bucket_props(Nodes, Type) when is_list(Nodes) ->    
+get_current_bucket_props(Nodes, Type) when is_list(Nodes) ->
     Node = lists:nth(length(Nodes), Nodes),
     get_current_bucket_props(Node, Type);
 get_current_bucket_props(Node, Type) when is_atom(Node) ->
-    rpc:call(Node, 
+    rpc:call(Node,
              riak_core_bucket_type,
              get,
              [Type]).
