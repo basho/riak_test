4c894425d45cef65862d6efd47da8b45dd13f7d3
Kelly McLaughlin <kelly@basho.com>
2014-04-15 16:43:05 -0600

    Add verify state

--- tests/verify_listkeys_eqc.erl
+++ tests/verify_listkeys_eqc.erl
@@ -34,27 +34,24 @@
 -define(BUCKET, <<"BUCKET">>).
 -define(NUM_KEYS, 5).
 -define(NUM_BUCKETS, 2).
--define(N, 3).
--define(R, 2).
-
 -define(NUM_TESTS, 4).
--define(RING_SIZE, 16).
 -define(LAZY_TIMER, 20).
--define(MANAGER, riak_core_metadata_manager).
 -define(PREFIX, {x, x}).
 -define(DEVS(N), lists:concat(["dev", N, "@127.0.0.1"])).
 -define(DEV(N), list_to_atom(?DEVS(N))).
 -define(THRESHOLD_SECS, 10).
 
 -record(state, {
-                node_joining = undefined,
-                nodes_up = [],
-                nodes_down = [],
-                nodes_ready_count = 0,
-                cluster_nodes = [],
-                ring_size = 0,
-                num_keys = 0
-                }).
+          bucket = undefined,
+          node_joining = undefined,
+          nodes_up = [],
+          nodes_down = [],
+          nodes_ready_count = 0,
+          cluster_nodes = [],
+          ring_size = 0,
+          num_keys = 0,
+          preload_complete = false
+         }).
 
 -record(node, {name, context}).
 
@@ -104,15 +101,11 @@ prop_test() ->
                           fun({node, N, _}) ->
                                   lager:info("Wiping out node ~p for good", [N]),
                                   rt:clean_data_dir(N),
-                                  %% rt:brutal_kill(N)
                                   rt:stop(N)
-                                  %% Would like to wipe out dirs after stopping, but
-                                  %% currently we use rpc to do that so it fails.
                           end,
                       Nodes = lists:usort(NU ++ CN),
                       lager:info("======================== Taking all nodes down ~p", [Nodes]),
                       lists:foreach(Destroy, Nodes),
-                      %% _ = rt:teardown(),
                       eqc_gen:with_parameter(show_states,
                                              true,
                                              pretty_commands(?MODULE, Cmds, {H, S, R}, equals(ok, R)))
@@ -131,8 +124,7 @@ add_nodes_pre(S, _) ->
     S#state.nodes_up == [].
 
 add_nodes_args(_S) ->
-    ?LET(Num, gen_numnodes(),
-    [Num]).
+    ?LET(Num, gen_numnodes(), [Num]).
 
 add_nodes(NumNodes) ->
     lager:info("Deploying cluster of size ~p", [NumNodes]),
@@ -150,8 +142,11 @@ add_nodes_post(_S, _Args, ok) ->
 add_nodes_post(_S, _Args, _) ->
     false.
 
-preload_pre(S) ->
-    S#state.nodes_up /= [].
+preload_pre(S) when S#state.nodes_up /= [],
+                    S#state.preload_complete == false ->
+    true;
+preload_pre(_) ->
+    false.
 
 preload_args(S) ->
     [g_uuid(), S#state.nodes_up, num_keys()].
@@ -163,17 +158,32 @@ preload(Bucket, Nodes, NumKeys) ->
     lager:info("Writing to bucket ~p", [Bucket]),
     %% @TODO Make a separate property to test bucket listing
     %% put_buckets(Node, ?NUM_BUCKETS),
-    %% @TODO Verify that puts have
-    %% completed using similar wait as in repl_bucket_types test
     put_keys(NodeName, Bucket, NumKeys).
 
-preload_next(S, _, [_, _, NumKeys]) ->
-    S#state{ num_keys = NumKeys }.
+preload_next(S, _, [Bucket, _, NumKeys]) ->
+    S#state{bucket = Bucket,
+            num_keys = NumKeys,
+            preload_complete=true}.
 
-preload_post(_S, [Bucket, Nodes, NumKeys], _R) ->
-    lager:info("In preload_post, Bucket:~p, Nodes:~p, NumKeys:~p", [Bucket, Nodes, NumKeys]),
-    KeyRes = [ list_keys(Node, Bucket, NumKeys, true) || {node, Node, _} <- Nodes ],
-    false == lists:member(false, KeyRes).
+preload_post(_S, [_Bucket, _Nodes, _NumKeys], _R) ->
+    %% lager:info("In preload_post, Bucket:~p, Nodes:~p, NumKeys:~p", [Bucket, Nodes, NumKeys]),
+    %% KeyRes = [ list_keys(Node, Bucket, NumKeys, true) || {node, Node, _} <- Nodes ],
+    %% false == lists:member(false, KeyRes).
+    true.
+
+verify_pre(#state{preload_complete=true}) ->
+    true;
+verify_pre(_) ->
+    false.
+
+verify_args(S) ->
+    [S#state.bucket, S#state.nodes_up, S#state.num_keys].
+
+verify(Bucket, Nodes, NumKeys) ->
+    [list_keys(Node#node.name, Bucket, NumKeys, true) || Node <- Nodes].
+
+verify_post(_S, _Args, R) ->
+    false == lists:member(false, R).
 
 %% ====================================================================
 %% Helpers
