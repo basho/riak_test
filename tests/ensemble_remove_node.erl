106f0608ae15810ed1aeb38742c97228eecbe5e2
Andrew J. Stone <andrew.j.stone.1@gmail.com>
2014-04-16 17:15:19 -0400

    add ensemble_remove_node

---
+++ tests/ensemble_remove_node.erl
@@ -0,0 +1,48 @@
+%% -------------------------------------------------------------------
+%%
+%% Copyright (c) 2013-2014 Basho Technologies, Inc.
+%%
+%% This file is provided to you under the Apache License,
+%% Version 2.0 (the "License"); you may not use this file
+%% except in compliance with the License.  You may obtain
+%% a copy of the License at
+%%
+%%   http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing,
+%% software distributed under the License is distributed on an
+%% "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+%% KIND, either express or implied.  See the License for the
+%% specific language governing permissions and limitations
+%% under the License.
+%%
+%% -------------------------------------------------------------------
+
+-module(ensemble_remove_node).
+-export([confirm/0]).
+-include_lib("eunit/include/eunit.hrl").
+
+confirm() ->
+    NumNodes = 3,
+    NVal = 3,
+    Config = ensemble_util:fast_config(NVal),
+    lager:info("Building cluster and waiting for ensemble to stablize"),
+    Nodes = ensemble_util:build_cluster(NumNodes, Config, NVal),
+    [Node, Node2, Node3] = Nodes,
+    ok = ensemble_util:wait_until_stable(Node, NVal),
+    lager:info("Store a value in the root ensemble"),
+    {ok, _} = riak_ensemble_client:kput_once(Node, root, testerooni, 
+        testvalue, 1000),
+    lager:info("Read that value from the root ensemble"),
+    {ok, _} = riak_ensemble_client:kget(Node, root, testerooni, 1000),
+    lager:info("Removing Nodes 2 and 3 from the cluster"),
+    rt:leave(Node2),
+    rt:leave(Node3),
+    Remaining = Nodes -- [Node2, Node3],
+    rt:wait_until_nodes_agree_about_ownership(Remaining), 
+    ok = rt:wait_until_unpingable(Node2),
+    ok = rt:wait_until_unpingable(Node3),
+    ok = ensemble_util:wait_for_membership(Node),
+    lager:info("Successfully read that value from the root ensemble"),
+    {ok, _Obj} = riak_ensemble_client:kget(Node, root, testerooni, 1000),
+    pass.
