e966ecfbe75d6d634f6308cfa106365e2133c323
Kelly McLaughlin <kelly@basho.com>
2014-05-12 15:03:04 -0600

    Fix client_java_verify failures
    The client_java_verify test has failed for quite some time with a
    series of java.lang.NoClassDefFoundError errors. There seems to be an
    issue with the use or generation of the jar file that includes all
    needed dependencies for the test to run properly. Change the test
    execution to use maven as is prescribed in the java client
    documentation and add a dependency on the java client repository to
    riak_test.

--- tests/client_java_verify.erl
+++ tests/client_java_verify.erl
@@ -3,22 +3,19 @@
 -export([confirm/0]).
 -include_lib("eunit/include/eunit.hrl").
 
-%% Change when a new release comes out.
--define(JAVA_FAT_BE_URL, rt_config:get('java.fat_be_url')).
--define(JAVA_TESTS_URL, rt_config:get('java.tests_url')).
-
 -prereq("java").
+-prereq("mvn").
 -prereq("curl").
 
 confirm() ->
-    
+
     lager:info("+P ~p", [erlang:system_info(process_limit)]),
     prereqs(),
     Nodes = rt:deploy_nodes(1),
     [Node1] = Nodes,
     ?assertEqual(ok, rt:wait_until_nodes_ready([Node1])),
-    
-    rpc:call(Node1, application, set_env, [erlang_js, script_timeout, 10000]), 
+
+    rpc:call(Node1, application, set_env, [erlang_js, script_timeout, 10000]),
     [{Node1, ConnectionInfo}] = rt:connection_info([Node1]),
     {HTTP_Host, HTTP_Port} = orddict:fetch(http, ConnectionInfo),
     {PB_Host, PB_Port} = orddict:fetch(pb, ConnectionInfo),
@@ -29,23 +26,19 @@ confirm() ->
     pass.
 
 prereqs() ->
-    %% Does you have the java client available?
-    rt_local:download(?JAVA_FAT_BE_URL),
-    rt_local:download(?JAVA_TESTS_URL),
     ok.
 
 java_unit_tests(HTTP_Host, HTTP_Port, _PB_Host, PB_Port) ->
     lager:info("Run the Java unit tests from somewhere on the local machine."),
-
     %% run the following:
     Cmd = io_lib:format(
-        "java -Dcom.basho.riak.host=~s -Dcom.basho.riak.http.port=~p -Dcom.basho.riak.pbc.port=~p -cp ~s:~s org.junit.runner.JUnitCore com.basho.riak.client.AllTests",
-        [HTTP_Host, HTTP_Port, PB_Port, 
-        rt_config:get(rt_scratch_dir) ++ "/" ++ rt_local:url_to_filename(?JAVA_FAT_BE_URL), 
-        rt_config:get(rt_scratch_dir) ++ "/" ++ rt_local:url_to_filename(?JAVA_TESTS_URL)]),
+            "mvn -f deps/riak_java_client/pom.xml -Pitest clean compile verify "
+            "-DargLine=\"-Dcom.basho.riak.host=~s -Dcom.basho.riak.http.port=~p"
+            " -Dcom.basho.riak.pbc.port=~p\"",
+            [HTTP_Host, HTTP_Port, PB_Port]),
     lager:info("Cmd: ~s", [Cmd]),
 
-    {ExitCode, JavaLog} = rt_local:stream_cmd(Cmd, [{cd, rt_config:get(rt_scratch_dir)}]),
+    {ExitCode, JavaLog} = rt_local:stream_cmd(Cmd, []),
     ?assertEqual(0, ExitCode),
     lager:info(JavaLog),
     ?assertNot(rt:str(JavaLog, "FAILURES!!!")),
