5d9db3750336c0114d18ee4578888ed2a352f9d5
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2014-05-30 13:50:14 -0400

    Add prototype test to generate memory leak.

---
+++ tests/repl_rt_heartbeat_interval.erl
@@ -0,0 +1,112 @@
+-module(repl_rt_heartbeat_interval).
+-behavior(riak_test).
+-export([confirm/0]).
+-include_lib("eunit/include/eunit.hrl").
+
+-define(TEST_BUCKET,
+        <<"repl-cancel-fullsync-failures-systest_a">>).
+-define(NUM_KEYS, 1000).
+
+-define(CONF(Retries), [
+        {riak_core,
+            [
+             {ring_creation_size, 8},
+             {default_bucket_props, [{n_val, 1}]}
+            ]
+        },
+        {riak_kv,
+            [
+             %% Specify fast building of AAE trees
+             {anti_entropy, {on, []}},
+             {anti_entropy_build_limit, {100, 1000}},
+             {anti_entropy_concurrency, 100}
+            ]
+        },
+        {riak_repl,
+         [
+          {fullsync_strategy, keylist},
+          {fullsync_on_connect, false},
+          {fullsync_interval, disabled},
+          {max_fssource_retries, Retries},
+          {rt_heartbeat_interval, disabled}
+         ]}
+        ]).
+
+%% @doc Ensure we can cancel a fullsync and restart it.
+confirm() ->
+    rt:set_advanced_conf(all, ?CONF(5)),
+
+    Nodes = [ANodes, BNodes] = rt:build_clusters([3, 3]),
+
+    lager:info("ANodes: ~p", [ANodes]),
+    lager:info("BNodes: ~p", [BNodes]),
+
+    AFirst = hd(ANodes),
+    BFirst = hd(BNodes),
+
+    lager:info("Naming clusters."),
+    repl_util:name_cluster(AFirst, "A"),
+    repl_util:name_cluster(BFirst, "B"),
+
+    lager:info("Waiting for convergence."),
+    rt:wait_until_ring_converged(ANodes),
+    rt:wait_until_ring_converged(BNodes),
+
+    lager:info("Waiting for transfers to complete."),
+    rt:wait_until_transfers_complete(ANodes),
+    rt:wait_until_transfers_complete(BNodes),
+
+    lager:info("Get leaders."),
+    LeaderA = repl_util:get_leader(AFirst),
+    LeaderB = repl_util:get_leader(BFirst),
+
+    lager:info("Finding connection manager ports."),
+    BPort = repl_util:get_port(LeaderB),
+
+    lager:info("Connecting cluster A to B"),
+    repl_util:connect_cluster_by_name(LeaderA, BPort, "B"),
+
+    repl_util:write_to_cluster(AFirst, 1, ?NUM_KEYS, ?TEST_BUCKET),
+
+    repl_util:read_from_cluster(BFirst, 1, ?NUM_KEYS, ?TEST_BUCKET,
+                                ?NUM_KEYS),
+
+    lager:info("Test fullsync from cluster A leader ~p to cluster B",
+               [LeaderA]),
+    repl_util:enable_fullsync(LeaderA, "B"),
+    rt:wait_until_ring_converged(ANodes),
+
+    lager:info("Enabling realtime replication."),
+    repl_util:enable_realtime(LeaderA, "B"),
+    rt:wait_until_ring_converged(ANodes),
+
+    lager:info("Starting realtime replication."),
+    repl_util:start_realtime(LeaderA, "B"),
+    rt:wait_until_ring_converged(ANodes),
+
+    Me = self(),
+
+    spawn(fun() ->
+                repl_util:write_to_cluster(AFirst,
+                                           10000, 20000, ?TEST_BUCKET),
+                Me ! complete
+        end),
+
+    wait_for_keys(AFirst),
+
+    rt:log_to_nodes(Nodes, "Test completed."),
+
+    rt:clean_cluster(ANodes),
+    rt:clean_cluster(BNodes),
+
+    pass.
+
+wait_for_keys(Node) ->
+    receive
+        complete ->
+            lager:info("Done!")
+    after 10 ->
+            Result = rpc:call(Node, erlang, memory, [binary]),
+            lager:info("Result: ~p", [Result]),
+            wait_for_keys(Node)
+    end.
