bd6c55dc2ca4b68eb96b196da2070ff702aa43eb
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2014-07-15 13:18:39 -0400

    Update consistent_get_put to the new API.

--- perf/consistent_get_put.erl
+++ perf/consistent_get_put.erl
@@ -4,37 +4,39 @@
 
 -define(HARNESS, (rt_config:get(rt_harness))).
 
-%% note that this test does not currently work
-
 confirm() ->
     HostList = rt_config:get(rt_hostnames),
     Count = length(HostList),
-    BinSize = rt_config:get(perf_binsize),
+    BinSize = rt_config:get(perf_bin_size),
 
     Config = rtperf:standard_config(Count),
+    lager:info("Generated configuration is: ~p", [Config]),
 
-    Nodes = rtperf:build_cluster(Config),
+    [Nodes] = rt:build_clusters([Count]),
+    lager:info("Built cluster: ~p", [Nodes]),
 
     SetSize = rtperf:target_size(rt_config:get(perf_target_pct),
                                  BinSize,
                                  rt_config:get(perf_ram_size),
                                  Count),
     [Node | _] = Nodes,
-    rt:create_and_activate_bucket_type(Node, <<"sc">>, [{consistent, true}]),
+    rt:create_and_activate_bucket_type(Node,
+                                       <<"sc">>,
+                                       [{consistent, true}]),
 
     TestConfig =
         rt_bench:config(
           max,
           rt_config:get(perf_duration),
-          HostList,
-          {int_to_bin_bigendian, {uniform_int, SetSize}},
+          [{Host, 10017} || Host <- HostList],
+          {int_to_bin_bigendian, {truncated_pareto_int, SetSize}},
           rt_bench:valgen(rt_config:get(perf_bin_type), BinSize),
           %% 4:1 get/put
           [{get, 3}, {update, 1}],
-      {<<"sc">>, <<"testbucket">>}
-         ),
+          {<<"sc">>, <<"testbucket">>},
+          riakc_pb),
 
-    ok = rtperf:maybe_prepop(HostList, BinSize, SetSize),
+    ok = rtperf:maybe_prepop(Nodes, BinSize, SetSize),
 
-    ok = rtperf:run_test(HostList, TestConfig, []),
+    ok = rtperf:run_test(Nodes, TestConfig, []),
     pass.
