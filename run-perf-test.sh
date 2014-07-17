e38c5423aa1d9a7d6158e4f5b8e152f85914d500
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2014-07-17 14:43:45 -0400

    Only run CRDT bench by default.

--- run-perf-test.sh
+++ run-perf-test.sh
@@ -5,10 +5,10 @@ make clean
 make
 
 echo "Running the CRDT team benchmark."
-./riak_test -c rtperf -t crdt_team_map -v -- --run-time 20 --ram-size 61440 --name crdt_team_map
+./riak_test -c rtperf -t crdt_team_map -v -- --run-time 10 --ram-size 61440 --name crdt_team_map
 
-echo "Running the get/put benchmark."
-./riak_test -c rtperf -t get_put -v -- --run-time 20 --ram-size 61440 --name get_put
+# echo "Running the get/put benchmark."
+# ./riak_test -c rtperf -t get_put -v -- --run-time 10 --ram-size 61440 --name get_put
 
-echo "Running the consistent get/put benchmark."
-./riak_test -c rtperf -t consistent_get_put -v -- --run-time 20 --ram-size 61440 --name consistent_get_put
+# echo "Running the consistent get/put benchmark."
+# ./riak_test -c rtperf -t consistent_get_put -v -- --run-time 10 --ram-size 61440 --name consistent_get_put
