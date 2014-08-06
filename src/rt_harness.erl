7a3f0f9de812fb10dec9f1c78d532cd0730ef866
Jon Anderson <janderson@basho.com>
2014-08-06 07:36:21 -0400

    Fix whitespace problems; add some edoc to harness modules.

--- src/rt_harness.erl
+++ src/rt_harness.erl
@@ -17,6 +17,10 @@
 %% under the License.
 %%
 %% -------------------------------------------------------------------
+%% @doc rt_harness provides a level of indirection between the modules
+%% calling into the harness and the configured harness, resolving the call
+%% to the configured harness. Calls such as 'rt_harness:start(Node)' will
+%% be resolved to the configured harness.
 -module(rt_harness).
 
 -define(HARNESS_MODULE, (rt_config:get(rt_harness))).
