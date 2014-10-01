04278377faee43c8d46e09e1c35a070396775122
Sean Cribbs <sean@basho.com>
2014-10-01 09:02:41 -0500

    WIP new console test for riak-repl

--- tests/replication2_console_tests.erl
+++ tests/replication2_console_tests.erl
@@ -48,8 +48,72 @@
 confirm() ->
     %% Deploy a node to test against
     lager:info("Deploy node to test riak-repl command line"),
-    [Node] = rt:deploy_nodes(1, [], [riak_kv, riak_repl]),
-    ?assertEqual(ok, rt:wait_until_nodes_ready([Node])),
+    [Node] = rt:deploy_nodes(1),
+    ?assertEqual(ok, (rt:wait_until_nodes_ready([Node]))),
+
+    %% Check for usage printouts on incorrect command arities
+    check_usage(Node, "modez"), %% modes takes any number of arguments so this checks the top level
+    check_usage(Node, "status ERHT"),
+    check_usage(Node, "add-listener Are you"),
+    check_usage(Node, "add-nat-listener having a problem"),
+    check_usage(Node, "del-listener buying a"),
+    check_usage(Node, "add-site home or"),
+    check_usage(Node, "del-site something Fret no more"),
+    check_usage(Node, "start-fullsync Hi there"),
+    check_usage(Node, "cancel-fullsync Belindas"),
+    check_usage(Node, "pause-fullsync Im Senor Cardgage"),
+    check_usage(Node, "resume-fullsync for Senor Cardgage"),
+    check_usage(Node, "resume-fullsync Mortgage"),
+    check_usage(Node, "clusterstats We can help you get a leg up"),
+    check_usage(Node, "clustername on the pile"),
+    check_usage(Node, "connections Low rates"),
+    check_usage(Node, "clusters percent signs"),
+    check_usage(Node, "connect I dunno"),
+    check_usage(Node, "disconnect Bad credit"),
+    check_usage(Node, "realtime enable No probalo"),
+    check_usage(Node, "realtime disable Home Lawn"),
+    check_usage(Node, "realtime start Escrow Re-Financin"),
+    check_usage(Node, "realtime stop You name it we got it"),
+    check_usage(Node, "realtime cascades Come along down for a free canceltation"),
+    check_usage(Node, "fullsync enable with one of our handsome talking experts"),
+    check_usage(Node, "fullsync disable One o them said"),
+    check_usage(Node, "fullsync start theyd buy me lunch"),
+    check_usage(Node, "fullsync stop But I dont see nobody"),
+    check_usage(Node, "fullsync source taking me to"),
+    check_usage(Node, "fullsync sink Chick-Fil-A"),
+    check_usage(Node, "proxy-get enable Senor Cardgage Mortgage"),
+    check_usage(Node, "proxy-get disable helped consolidate my whole life"),
+    check_usage(Node, "proxy-get redirect show into this tiny box"),
+    check_usage(Node, "proxy-get redirect add Thanks Senor Mortgage"),
+    check_usage(Node, "proxy-get redirect delete Youre every welcome Valerie"),
+    check_usage(Node, "proxy-get redirect cluster-id Act now and see"),
+    check_usage(Node, "nat-map show if you can stand"),
+    check_usage(Node, "nat-map add to talk to me for"),
+    check_usage(Node, "nat-map del more than four seconds"),
+    check_usage(Node, "Get a leg up on the pile and refinance your dreams"),
+
+    %% When repl12_mode is active, Version 2 commands are shown, when
+    %% repl13_mode is active Version 3 commands are shown.
+    lager:info("Checking V2 and V3 usage output"),
+    %% rpc:call(Node, riak_repl_console, set_modes, [repl12_mode, repl13_mode]),
+    rt:riak_repl(Node, ["modes mode_repl12 mode_repl13"]),
+    OutBoth = check_usage(Node, ""),
+    assertContains(OutBoth,"Version 2 Commands:"),
+    assertContains(OutBoth,"Version 3 Commands:"),
+
+    lager:info("Checking V2-only usage output"),
+    rt:riak_repl(Node, ["modes mode_repl12"]),
+    Out2 = check_usage(Node, ""),
+    assertContains(Out2, "Version 2 Commands:"),
+    assertNotContains(Out2, "Version 3 Commands:"),
+
+    lager:info("Checking V3-only usage output"),
+    rt:riak_repl(Node, ["modes mode_repl13"]),
+    Out3 = check_usage(Node, ""),
+    assertContains(Out3, "Version 3 Commands:"),
+    assertNotContains(Out3, "Version 2 Commands:"),
+
+    %% Install intercepts so the real commands aren't run
     rt_intercept:add(Node,
                      {riak_repl_console,
                       [
@@ -87,14 +151,14 @@ confirm() ->
     check_cmd(Node, "realtime cascades"),
     check_cmd(Node, "realtime cascades always"),
 
-    check_cmd(Node, "fullsync max_fssource_node"),
-    check_cmd(Node, "fullsync max_fssource_node 99"),
+    check_cmd(Node, "fullsync source max_workers_per_node"),
+    check_cmd(Node, "fullsync source max_workers_per_node 99"),
 
-    check_cmd(Node, "fullsync max_fssource_cluster"),
-    check_cmd(Node, "fullsync max_fssource_cluster 99"),
+    check_cmd(Node, "fullsync source max_workers_per_cluster"),
+    check_cmd(Node, "fullsync source max_workers_per_cluster 99"),
 
-    check_cmd(Node, "fullsync max_fssink_node"),
-    check_cmd(Node, "fullsync max_fssink_node 99"),
+    check_cmd(Node, "fullsync sink max_workers_per_node"),
+    check_cmd(Node, "fullsync sink max_workers_per_node 99"),
 
     check_cmd(Node, "fullsync enable foo"),
     check_cmd(Node, "fullsync disable bar"),
@@ -102,22 +166,37 @@ confirm() ->
     check_cmd(Node, "realtime enable foo"),
     check_cmd(Node, "realtime disable bar"),
 
-    check_cmd(Node, "proxy_get enable foo"),
-    check_cmd(Node, "proxy_get disable bar"),
+    check_cmd(Node, "proxy-get enable foo"),
+    check_cmd(Node, "proxy-get disable bar"),
 
     check_cmd(Node, "nat-map show"),
     check_cmd(Node, "nat-map add 1.2.3.4:4321 192.168.1.1"),
     check_cmd(Node, "nat-map del 1.2.3.4:4321 192.168.1.1"),
 
-    check_cmd(Node, "add-block-provider-redirect a b"),
-    check_cmd(Node, "show-block-provider-redirect a"),
-    check_cmd(Node, "delete-block-provider-redirect a"),
-    check_cmd(Node, "show-local-cluster-id"),
+    check_cmd(Node, "proxy-get redirect add a b"),
+    check_cmd(Node, "proxy-get redirect show a"),
+    check_cmd(Node, "proxy-get redirect del a"),
+    check_cmd(Node, "proxy-get redirect cluster-id"),
 
     pass.
 
 check_cmd(Node, Cmd) ->
-    lager:info("Testing riak-repl ~s on ~s", [Cmd, Node]),
+    lager:info("Testing `riak-repl ~s` on ~s", [Cmd, Node]),
     {ok, Out} = rt:riak_repl(Node, [Cmd]),
     ?assertEqual("pass", Out).
 
+check_usage(Node, Cmd) ->
+    lager:info("Checking usage output for `riak-repl ~s` on ~s", [Cmd, Node]), 
+    {ok, Out} = rt:riak_repl(Node, [Cmd]),
+    assertContains(Out, "(^|\n)Usage: "),
+    Out.
+
+assertContains(Output, Str) ->
+     ?assertEqual(match, (contains(Output, Str))).
+
+assertNotContains(Output, Str) ->
+     ?assertEqual(nomatch, (contains(Output, Str))).
+
+
+contains(Output, Str) ->
+    re:run(Output, Str, [{capture, none}, global]).
