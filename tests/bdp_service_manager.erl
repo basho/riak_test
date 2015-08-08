da17099b1991847fd553c71599b9c8af16374cff
Andrei Zavada <johnhommer@gmail.com>
2015-08-08 04:50:32 +0400

    WIP still figuring out
    what exactly needs to be done to have a node leave then join a cluster.

--- tests/bdp_service_manager.erl
+++ tests/bdp_service_manager.erl
@@ -39,55 +39,34 @@
 confirm() ->
     ClusterSize = 3,
     lager:info("Building cluster"),
-    _Nodes = [Node1, Node2, Node3] = build_cluster(ClusterSize),
+    _Nodes = [Node1, Node2, Node3] = bdp_util:build_cluster(ClusterSize),
 
     %% add a service
     ok = service_added(Node1, ?S1_NAME, ?S1_TYPE, ?S1_CONFIG),
-    ok = wait_services(Node1, {[], [?S1_NAME]}),
+    ok = bdp_util:wait_services(Node1, {[], [?S1_NAME]}),
     lager:info("Service ~p (~s) added", [?S1_NAME, ?S1_TYPE]),
 
-    ok = test_service_manager(Node1, Node1, Node1, "all on one"),
-    ok = test_service_manager(Node1, Node2, Node2, "start/stop on same"),
-    ok = test_service_manager(Node1, Node2, Node3, "one-two-three"),
+    ok = test_service_manager(Node1, Node1, Node1, "111"),
+    ok = test_service_manager(Node1, Node2, Node2, "122"),
+    ok = test_service_manager(Node1, Node2, Node3, "123"),
 
-    %% remove_ensemble_node(Node1, Node3),
-    %% ensemble_util:wait_until_cluster(Nodes -- [Node3]),
-    %% rt:leave(Node3),
-    %% rt:wait_until_no_pending_changes(Nodes -- [Node3]),
+    %% ok = bdp_util:make_node_leave(Node2, Node1),
+    %% rt:remove(Node1, Node2),
+    %% rt:start_and_wait(Node2),
+    %% %rt:join_cluster(Nodes),
+    %% ensemble_util:wait_until_cluster(Nodes),
+    %% ok = bdp_util:make_node_join(Node2, Node1),
+    %% ensemble_util:wait_until_cluster(Nodes),
 
-    ok = test_service_manager(Node1, Node1, Node1, "all on one"),
-    ok = test_service_manager(Node1, Node2, Node2, "start/stop on same"),
-    ok = test_service_manager(Node3, Node2, Node3, "3-two-3"),
-
-    %% rt:leave(Node2),
-    %% rt:wait_until_no_pending_changes(Nodes -- [Node3, Node2]),
-    %% remove_ensemble_node(Node1, Node2),
-    %% ensemble_util:wait_until_cluster(Nodes -- [Node3, Node2]),
-
-    %% ok = test_service_manager(Node1, Node1, Node1, "all on one"),
-    %% ok = test_service_manager(Node1, Node5, Node5, "one-five-five"),
-    %% ok = test_service_manager(Node1, Node5, Node3, "one-five-three"),
+    ok = test_service_manager(Node1, Node1, Node1, "111"),
+    ok = test_service_manager(Node1, Node2, Node2, "122"),
+    ok = test_service_manager(Node1, Node2, Node3, "123"),
 
     ok = service_removed(Node2, ?S1_NAME),
+    ok = bdp_util:wait_services(Node1, {[], []}),
     lager:info("Service removed"),
 
     pass.
-
-%% copied from ensemble_util.erl
-build_cluster(Size) ->
-    Nodes = rt:deploy_nodes(Size),
-    rt:join_cluster(Nodes),
-    ensemble_util:wait_until_cluster(Nodes),
-    lager:info("Waiting until stable"),
-    ensemble_util:wait_until_quorum(hd(Nodes), root),
-    lager:info("....is stable"),
-    Nodes.
-
-
-
-%% remove_ensemble_node(ByNode, NodeToRemove) ->
-%%     ok = rpc:call(ByNode, riak_ensemble_manager, remove,
-%%                   [ByNode, NodeToRemove]).
 
 
 
@@ -111,66 +90,6 @@ test_service_manager(NodeA, NodeB, NodeC, Desc) ->
     ok.
 
 
-get_services(Node) ->
-    {Running_, Available_} =
-        case rpc:call(Node, data_platform_global_state, services, []) of
-            {error, timeout} ->
-                lager:info("RPC call to ~p timed out", [Node]),
-                ?assert(false);
-            Result ->
-                Result
-        end,
-    {Running, Available} =
-        {lists:sort([SName || {_Type, SName, _Node} <- Running_]),
-         lists:sort([SName || {SName, _Type, _Conf} <- Available_])},
-    lager:debug("Services running: ~p, available: ~p", [Running, Available]),
-    {Running, Available}.
-
-wait_services(Node, Services) ->
-    wait_services_(Node, Services, 20).
-wait_services_(_Node, _Services, SecsToWait) when SecsToWait =< 0 ->
-    {error, services_not_ready};
-wait_services_(Node, Services, SecsToWait) ->
-    case get_services(Node) of
-        Services ->
-            ok;
-        _Incomplete ->
-            timer:sleep(1000),
-            wait_services_(Node, Services, SecsToWait - 1)
-    end.
-
-service_added(Node, ConfigName, ServiceType, Config) ->
-    {Rnn0, Avl0} = get_services(Node),
-    ok = rpc:call(Node, data_platform_global_state, add_service_config,
-                  [ConfigName, ServiceType, Config, false]),
-    Avl1 = lists:usort(Avl0 ++ [ConfigName]),
-    ok = wait_services(Node, {Rnn0, Avl1}).
-
-
-service_removed(Node, ConfigName) ->
-    {Rnn0, Avl0} = get_services(Node),
-    ok = rpc:call(Node, data_platform_global_state, remove_service,
-                  [ConfigName]),
-    Avl1 = lists:usort(Avl0 -- [ConfigName]),
-    ok = wait_services(Node, {Rnn0, Avl1}).
-
-
-service_started(Node, ServiceNode, Group, ConfigName) ->
-    {Rnn0, Avl0} = get_services(Node),
-    ok = rpc:call(Node, data_platform_global_state, start_service,
-                  [Group, ConfigName, ServiceNode]),
-    Rnn1 = lists:usort(Rnn0 ++ [ConfigName]),
-    ok = wait_services(Node, {Rnn1, Avl0}).
-
-
-service_stopped(Node, ServiceNode, Group, ConfigName) ->
-    {Rnn0, Avl0} = get_services(Node),
-    ok = rpc:call(Node, data_platform_global_state, stop_service,
-                  [Group, ConfigName, ServiceNode]),
-    Rnn1 = lists:usort(Rnn0 -- [ConfigName]),
-    ok = wait_services(Node, {Rnn1, Avl0}).
-
-
 test_cross_node_start_stop(ServiceNode, Node1, Node2) ->
     %% start it, on Node1
     ok = service_started(Node1, ServiceNode, ?S1_TYPE, ?S1_NAME),
@@ -180,3 +99,35 @@ test_cross_node_start_stop(ServiceNode, Node1, Node2) ->
     ok = service_stopped(Node2, ServiceNode, ?S1_TYPE, ?S1_NAME),
     lager:info("Service ~p down on ~p", [?S1_NAME, Node2]),
     ok.
+
+
+service_added(Node, ConfigName, ServiceType, Config) ->
+    {Rnn0, Avl0} = bdp_util:get_services(Node),
+    ok = rpc:call(Node, data_platform_global_state, add_service_config,
+                  [ConfigName, ServiceType, Config, false]),
+    Avl1 = lists:usort(Avl0 ++ [ConfigName]),
+    ok = bdp_util:wait_services(Node, {Rnn0, Avl1}).
+
+
+service_removed(Node, ConfigName) ->
+    {Rnn0, Avl0} = bdp_util:get_services(Node),
+    ok = rpc:call(Node, data_platform_global_state, remove_service,
+                  [ConfigName]),
+    Avl1 = lists:usort(Avl0 -- [ConfigName]),
+    ok = bdp_util:wait_services(Node, {Rnn0, Avl1}).
+
+
+service_started(Node, ServiceNode, Group, ConfigName) ->
+    {Rnn0, Avl0} = bdp_util:get_services(Node),
+    ok = rpc:call(Node, data_platform_global_state, start_service,
+                  [Group, ConfigName, ServiceNode]),
+    Rnn1 = lists:usort(Rnn0 ++ [ConfigName]),
+    ok = bdp_util:wait_services(Node, {Rnn1, Avl0}).
+
+
+service_stopped(Node, ServiceNode, Group, ConfigName) ->
+    {Rnn0, Avl0} = bdp_util:get_services(Node),
+    ok = rpc:call(Node, data_platform_global_state, stop_service,
+                  [Group, ConfigName, ServiceNode]),
+    Rnn1 = lists:usort(Rnn0 -- [ConfigName]),
+    ok = bdp_util:wait_services(Node, {Rnn1, Avl0}).
---
+++ tests/bdp_util.erl
@@ -0,0 +1,128 @@
+%% -------------------------------------------------------------------
+%%
+%% Copyright (c) 2015 Basho Technologies, Inc.
+%%
+%% This file is provided to you under the Apache License,
+%% Version 2.0 (the "License"); you may not use this file
+%% except in compliance with the License.  You may obtain
+%% a copy of the License at
+%%
+%%   http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing,
+%% software distributed under the License is distributed on an
+%% "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+%% KIND, either express or implied.  See the License for the
+%% specific language governing permissions and limitations
+%% under the License.
+%%
+%% -------------------------------------------------------------------
+%% @doc Helper functions for bdp_service_manager.erl and others.
+
+-module(bdp_util).
+
+-export([build_cluster/1,
+         make_node_leave/2, make_node_join/2]).
+-export([get_services/1, wait_services/2]).
+
+-include_lib("eunit/include/eunit.hrl").
+
+
+%% copied from ensemble_util.erl
+build_cluster(Size) ->
+    Nodes = rt:deploy_nodes(Size),
+    rt:join_cluster(Nodes),
+    ensemble_util:wait_until_cluster(Nodes),
+    Nodes.
+
+
+get_services(Node) ->
+    {Running_, Available_} =
+        case rpc:call(Node, data_platform_global_state, services, []) of
+            {error, timeout} ->
+                lager:info("RPC call to ~p timed out", [Node]),
+                ?assert(false);
+            Result ->
+                Result
+        end,
+    {Running, Available} =
+        {lists:sort([SName || {_Type, SName, _Node} <- Running_]),
+         lists:sort([SName || {SName, _Type, _Conf} <- Available_])},
+    lager:debug("Services running: ~p, available: ~p", [Running, Available]),
+    {Running, Available}.
+
+wait_services(Node, Services) ->
+    wait_services_(Node, Services, 20).
+wait_services_(_Node, _Services, SecsToWait) when SecsToWait =< 0 ->
+    {error, services_not_ready};
+wait_services_(Node, Services, SecsToWait) ->
+    case get_services(Node) of
+        Services ->
+            ok;
+        _Incomplete ->
+            timer:sleep(1000),
+            wait_services_(Node, Services, SecsToWait - 1)
+    end.
+
+
+
+%% We also provide a makeshift BDP SM API emulating data-platform-admin commands
+
+make_node_leave(Node, FromNode) ->
+    lager:info("away goes ~p", [Node]),
+    rt:wait_until(
+      fun() -> ok == do_leave(Node, FromNode) end).
+
+do_leave(Node, FromNode) ->
+    F = fun(M, F, A) -> rpc:call(FromNode, M, F, A) end,
+    RootLeader = F(riak_ensemble_manager, rleader_pid, []),
+    case F(riak_ensemble_peer, update_members, [RootLeader, [{del, {root, Node}}], 10000]) of
+        ok ->
+            case wait_for_root_leave(F, Node, 30) of
+                ok ->
+                    NewRootLeader = F(riak_ensemble_manager, rleader_pid, []),
+                    case F(riak_ensemble_manager, remove, [node(NewRootLeader), Node]) of
+                        ok ->
+                            ok;
+                        Error ->
+                            Error
+                    end;
+                Error ->
+                    Error
+            end;
+        Error ->
+            Error
+    end.
+
+wait_for_root_leave(F, Node, Timeout) ->
+    wait_for_root_leave(F, Node, 0, Timeout).
+
+wait_for_root_leave(_F, _Node, RetryCount, RetryLimit) when RetryCount =:= RetryLimit ->
+    {error, timeout_waiting_to_leave_root_ensemble};
+wait_for_root_leave(F, Node, RetryCount, RetryLimit) ->
+    case in_root_ensemble(F, Node) of
+        true ->
+            timer:sleep(1000),
+            wait_for_root_leave(F, Node, RetryCount + 1, RetryLimit);
+        false ->
+            ok
+    end.
+
+in_root_ensemble(F, Node) ->
+    RootNodes = [N || {root, N} <- F(riak_ensemble_manager, get_members, [root])],
+    lists:member(Node, RootNodes).
+
+
+make_node_join(Node, ToNode) ->
+    lager:info("Join ~p", [Node]),
+    %% ring-readiness and transfer-complete status is of no
+    %% consequence for our tests, so just proceed
+    case rpc:call(ToNode, riak_ensemble_manager, join, [Node, ToNode]) of
+        ok ->
+            ok;
+        remote_not_enabled ->
+            %% it's an error, in the context of a test run
+            lager:error("Ensemble not enabled on node ~p", [Node]);
+        Error ->
+            lager:error("Joining node ~p from ~p failed: ~p", [Node, ToNode, Error])
+    end.
