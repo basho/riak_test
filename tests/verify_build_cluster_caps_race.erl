6f29ae93f689b1740cc98ee18ccb761be3e0e439
John R. Daily <git@epep.us>
2015-08-10 23:35:24 -0400

    Test reliably passes locally, do not need gossip, just a slow riak_core start

--- tests/verify_build_cluster_caps_race.erl
+++ tests/verify_build_cluster_caps_race.erl
@@ -25,64 +25,31 @@
 -import(rt, [wait_until_nodes_ready/1,
              wait_until_no_pending_changes/1]).
 
--define(NEVER_LIMIT, {0, 1000000000}).
--define(ONCE_LIMIT, {1, 1000000000}).
--define(STORM_LIMIT, {1000, 10000}). % 1000 / 10 secs
+%% We have to define our own deploy_nodes to force a race condition
+-define(HARNESS, (rt_config:get(rt_harness))).
+
+deploy_nodes(InitialConfig) ->
+    NodeConfig = [{current, Config} || Config <- InitialConfig],
+    Nodes = ?HARNESS:deploy_nodes(NodeConfig),
+    lager:info("Start nodes ~p without waiting for services", [Nodes]),
+    Nodes.
+
+staged_join(InitiatingNode, DestinationNode) ->
+    rpc:call(InitiatingNode, riak_core, staged_join,
+             [DestinationNode]).
 
 confirm() ->
     %% Deploy a set of new nodes
     lager:info("Deploying nodes"),
 
-    %% Disable gossip by starving it of tokens.
-    %% Gossip messages can be triggered by sending 'reset_tokens'
-    %% message to riak_core_gossip
-
+    %% We want riak_core to be slow to start on node 2 to verify that
+    %% the join will be disallowed if init is not yet complete
     Configs = [
-               [{riak_core, [{gossip_limit, ?NEVER_LIMIT}]}],
-               [{riak_core, [{gossip_limit, ?NEVER_LIMIT},{delayed_start, 2500}]}]
+               [{riak_core, []}],
+               [{riak_core, [{delayed_start, 2500}]}]
               ],
-    [Node1, Node2] = Nodes = rt:deploy_nodes(2, Configs),
+    [Node1, Node2] = deploy_nodes(Configs),
 
-    %% Speed failure up a bit
-    rt_config:set(rt_max_wait_time, timer:seconds(120)),
-    rt_config:set(rt_retry_delay, 500),
-
-    lager:info("joining Node 2 to the cluster and limiting gossip..."),
-    ok = rt:staged_join(Node2, Node1),
-    give_token(Node2), % allow the join message to reach
-    ok = rt:plan_and_commit(Node1),
-
-    %% lager:info("setting bogus capability on both sides"),
-    %% [set_bogus_cap(N) || N <- Nodes],
-
-    %% Make sure the ring is *not* ready on all nodes
-    [?assertEqual(false, rt:is_ring_ready(N)) || N <- Nodes],
-
-    lager:info("re-enabling gossip"),
-    [set_gossip_limit(N, ?STORM_LIMIT) || N <- Nodes],
-
-    %% Look for a happy cluster
-    ?assertEqual(ok, rt:wait_until_nodes_ready(Nodes)),
-    ?assertEqual(ok, rt:wait_until_nodes_agree_about_ownership(Nodes)),
-    ?assertEqual(ok, wait_until_no_pending_changes(Nodes)),
-
+    lager:info("joining Node 2 to the cluster..."),
+    ?assertMatch({error, _}, staged_join(Node2, Node1)),
     pass.
-
-%% gossip(From, To) ->
-%%     %% First, give the lucky node a token
-%%     ok = give_token(From),
-
-%%     %% Then trigger the gossip
-%%     rpc:call(From, riak_core_gossip, send_ring_to, [From, To]).
-
-give_token(From) ->
-    set_gossip_limit(From, ?ONCE_LIMIT),
-    ok.
-
-set_gossip_limit(Node, Limit) ->
-    lager:info("Setting ~p gossip limit to ~p\n", [Node, Limit]),
-    ok = rpc:call(Node, application, set_env, [riak_core, gossip_limit, Limit]),
-    reset_tokens = rpc:call(Node, erlang, send, [{riak_core_gossip, Node}, reset_tokens]).
-
-%% set_bogus_cap(Node) ->
-%%     ok = rpc:call(Node, riak_core_capability, register, [{riak_core, herding},[goats,cats],cats]).
