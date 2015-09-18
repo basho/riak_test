87aca6bf1a6337e1f58c081e32c73865dda54034
Andrei Zavada <azavada@contractor.basho.com>
2015-09-18 13:28:35 +0400

    fixes to be squashed to tests/ts_basic

--- tests/ts_basic.erl
+++ tests/ts_basic.erl
@@ -43,11 +43,12 @@ confirm() ->
                  "CREATE TABLE ~s "
                  "(~s varchar not null, "
                  " ~s timestamp not null, "
-                 " money float not null, "
+                 " score float not null, "
                  " PRIMARY KEY((quantum(time, 10, s)), time, sensor))",
                  [?BUCKET, ?PKEY_P1, ?PKEY_P2]),
     Props = io_lib:format("{\\\"props\\\": {\\\"n_val\\\": 3, \\\"table_def\\\": \\\"~s\\\"}}", [TableDef]),
     rt:admin(Node1, ["bucket-type", "create", binary_to_list(?BUCKET), lists:flatten(Props)]),
+    rt:admin(Node1, ["bucket-type", "activate", binary_to_list(?BUCKET)]),
 
     %% 2. set up a client
     C = rt:pbc(Node1),
@@ -55,7 +56,8 @@ confirm() ->
 
     %% 3. put some data
     Data = make_data(),
-    riakc_ts:put(C, ?BUCKET, Data),
+    Res = riakc_ts:put(C, ?BUCKET, Data),
+    io:format("Put ~b records (~p)\n", [length(Data), Res]),
 
     %% 4. select
     Query =
@@ -63,15 +65,26 @@ confirm() ->
           io_lib:format(
             "select * from ~s where ~s > 19 and ~s < 80 and sensor = \"~s\"",
            [?BUCKET, ?PKEY_P2, ?PKEY_P2, ?PKEY_P1])),
-    {ok, Selected} = riakc_ts:query(C, Query),
+    {ok, Selected} = fetch_with_patience(C, Query, 10),
     ?assert(length(Selected) == 60),
 
     pass.
 
+fetch_with_patience(_C, Query, 0) ->
+    io:format("Failed waiting for fetch on query ~p", [Query]),
+    {error, fetch_timed_out};
+fetch_with_patience(C, Query, Tries) ->
+    case riakc_ts:query(C, Query) of
+        {ok, Selected} ->
+            {ok, Selected};
+        NotOk ->
+            io:format("Fetch attempt ~b failed: ~p\n", [10 - Tries, NotOk]),
+            timer:sleep(1000),
+            fetch_with_patience(C, Query, Tries - 1)
+    end.
 
 
 %% @ignore
-%% copied from ensemble_util.erl
 -spec build_cluster(non_neg_integer()) -> [node()].
 build_cluster(Size) ->
     build_cluster(Size, []).
@@ -79,9 +92,6 @@ build_cluster(Size) ->
 build_cluster(Size, Config) ->
     [_Node1|_] = Nodes = rt:deploy_nodes(Size, Config),
     rt:join_cluster(Nodes),
-    %% ensemble_util:wait_until_cluster(Nodes),
-    %% ensemble_util:wait_for_membership(Node1),
-    %%ensemble_util:wait_until_stable(Node1, Size),
     Nodes.
 
 
@@ -90,7 +100,7 @@ make_data() ->
     lists:foldl(
       fun(T, Q) ->
               [[<<"ZXC11">>,
-                ?LIFESPAN - T,
+                ?LIFESPAN - T + 1,
                 math:sin(float(T) / 100 * math:pi())] | Q]
       end,
-      [], lists:seq(0, ?LIFESPAN)).
+      [], lists:seq(?LIFESPAN, 0, -1)).
