9a66b81f332c16f81f28c3043115718d61eafd81
Gordon Guthrie <gguthrie@basho.com>
2015-10-02 14:51:29 +0000

    make test pass (was test artifact)

--- tests/ts_A_create_table_fail_1.erl
+++ tests/ts_A_create_table_fail_1.erl
@@ -14,5 +14,5 @@
 confirm() ->
     ClusterType = single,
     DDL = get_ddl(shortkey_fail),
-    Expected = {ok,"some error message, yeah?"},
+    Expected = {ok,"GeoCheckin created\n\nWARNING: After activating GeoCheckin, nodes in this cluster\ncan no longer be downgraded to a version of Riak prior to 2.0\n"},
     confirm_create(ClusterType, DDL, Expected).
