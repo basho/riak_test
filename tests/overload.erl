7af22bd969479e32541b6a767fb59243ea9fcd99
Doug Rohrer <drohrer@basho.com>
2015-10-22 14:03:25 -0400

    Clean up use of ConsistentType (use macros & pass BKV down to validate).

--- tests/overload.erl
+++ tests/overload.erl
@@ -31,9 +31,18 @@
 -define(GET_RETRIES, 1000).
 -define(BUCKET, <<"test">>).
 -define(KEY, <<"hotkey">>).
--define(NORMAL_BKV, {{NormalType, ?BUCKET}, ?KEY, <<"test">>}).
--define(CONSISTENT_BKV, {{ConsistentType, ?BUCKET}, ?KEY, <<"test">>}).
--define(WRITE_ONCE_BKV, {{WriteOnceType, ?BUCKET}, ?KEY, <<"test">>}).
+-define(NORMAL_TYPE, <<"normal_type">>).
+-define(CONSISTENT_TYPE, <<"consistent_type">>).
+-define(WRITE_ONCE_TYPE, <<"write_once_type">>).
+-define(NORMAL_BKV, {{?NORMAL_TYPE, ?BUCKET}, ?KEY, <<"test">>}).
+-define(CONSISTENT_BKV, {{?CONSISTENT_TYPE, ?BUCKET}, ?KEY, <<"test">>}).
+-define(WRITE_ONCE_BKV, {{?WRITE_ONCE_TYPE, ?BUCKET}, ?KEY, <<"test">>}).
+
+%% This record contains the default values for config settings if they were not set
+%% in the advanced.config file - because setting something to `undefined` is not the same
+%% as not setting it at all, we need to make sure to overwrite with defaults for each test,
+%% not just set things back to `undefined`. Also, makes the tests re-orderable as they always
+%% set everything they need, and don't depend on a previous test to make changes.
 -record(config, {
     vnode_overload_threshold = 10000,
     vnode_check_interval = 5000,
@@ -66,17 +75,12 @@ default_config(#config{
             {anti_entropy_timeout, 5000}]},
         {riak_api, [{pb_backlog, 1024}]}].
 
-
 confirm() ->
     Nodes = setup(),
 
-    NormalType = <<"normal_type">>,
-    ConsistentType = <<"consistent_type">>,
-    WriteOnceType = <<"write_once_type">>,
-
-    ok = create_bucket_type(Nodes, NormalType, [{n_val, 3}]),
-    ok = create_bucket_type(Nodes, ConsistentType, [{consistent, true}, {n_val, 5}]),
-    ok = create_bucket_type(Nodes, WriteOnceType, [{write_once, true}, {n_val, 1}]),
+    ok = create_bucket_type(Nodes, ?NORMAL_TYPE, [{n_val, 3}]),
+    ok = create_bucket_type(Nodes, ?CONSISTENT_TYPE, [{consistent, true}, {n_val, 5}]),
+    ok = create_bucket_type(Nodes, ?WRITE_ONCE_TYPE, [{write_once, true}, {n_val, 1}]),
     rt:wait_until(ring_manager_check_fun(hd(Nodes))),
 
 
@@ -87,39 +91,44 @@ confirm() ->
 
     Tests = [test_no_overload_protection,
              test_vnode_protection,
-             test_fsm_protection,
-             test_cover_queries_overload],
+             test_fsm_protection],
 
     [begin
          lager:info("Starting Test ~p for ~p~n", [Test, BKV]),
-         ok = erlang:apply(?MODULE, Test, [Nodes, BKV, IsConsistent])
+         ok = erlang:apply(?MODULE, Test, [Nodes, BKV])
      end || Test <- Tests,
-            {BKV, IsConsistent} <- [{?NORMAL_BKV, false},
-                                    {?CONSISTENT_BKV, true},
-                                    {?WRITE_ONCE_BKV, false}]],
+            BKV <- [?NORMAL_BKV,
+                    ?CONSISTENT_BKV,
+                    ?WRITE_ONCE_BKV]],
+    %% Test cover queries doesn't depend on bucket/keyvalue, just run it once
+    test_cover_queries_overload(Nodes),
     pass.
 
 
 setup() ->
     ensemble_util:build_cluster(5, default_config(), 5).
 
-test_no_overload_protection(_Nodes, _BKV, true) ->
+test_no_overload_protection(_Nodes, ?CONSISTENT_BKV) ->
     ok;
-test_no_overload_protection(Nodes, BKV, ConsistentType) ->
+test_no_overload_protection(Nodes, BKV) ->
+    lager:info("Setting default configuration for no overload protestion test."),
+    rt:pmap(fun(Node) ->
+        rt:update_app_config(Node, default_config())
+    end, Nodes),
     lager:info("Testing with no overload protection"),
     ProcFun = build_predicate_eq(test_no_overload_protection, ?NUM_REQUESTS,
                                   "ProcFun", "Procs"),
     QueueFun = build_predicate_gte(test_no_overload_protection, ?NUM_REQUESTS,
                                    "QueueFun", "Queue Size"),
-    verify_test_results(run_test(Nodes, BKV), ConsistentType, ProcFun, QueueFun).
+    verify_test_results(run_test(Nodes, BKV), BKV, ProcFun, QueueFun).
 
-verify_test_results({_NumProcs, QueueLen}, true, _, QueueFun) ->
+verify_test_results({_NumProcs, QueueLen}, ?CONSISTENT_BKV, _ProcFun, QueueFun) ->
     ?assert(QueueFun(QueueLen));
-verify_test_results({NumProcs, QueueLen}, false, ProcFun, QueueFun) ->
+verify_test_results({NumProcs, QueueLen}, _BKV, ProcFun, QueueFun) ->
     ?assert(ProcFun(NumProcs)),
     ?assert(QueueFun(QueueLen)).
 
-test_vnode_protection(Nodes, BKV, ConsistentType) ->
+test_vnode_protection(Nodes, BKV) ->
     %% Setting check_interval to one ensures that process_info is called
     %% to check the queue length on each vnode send.
     %% This allows us to artificially raise vnode queue lengths with dummy
@@ -134,7 +143,7 @@ test_vnode_protection(Nodes, BKV, ConsistentType) ->
             end, Nodes),
     ProcFun = build_predicate_lt(test_vnode_protection, (?NUM_REQUESTS+1), "ProcFun", "Procs"),
     QueueFun = build_predicate_lte(test_vnode_protection, (?THRESHOLD+1), "QueueFun", "QueueSize"),
-    verify_test_results(run_test(Nodes, BKV), ConsistentType, ProcFun, QueueFun),
+    verify_test_results(run_test(Nodes, BKV), BKV, ProcFun, QueueFun),
 
     [Node1 | _] = Nodes,
     CheckInterval = ?THRESHOLD div 2,
@@ -148,19 +157,19 @@ test_vnode_protection(Nodes, BKV, ConsistentType) ->
     Pid = suspend_vnode_proxy(Victim),
     ProcFun2 = build_predicate_gte("test_vnode_protection after suspend",
                                    (?NUM_REQUESTS), "ProcFun", "Procs"),
-    QueueFun2 = build_predicate_lt("test_vnode_protection after suspend",
+    QueueFun2 = build_predicate_lte("test_vnode_protection after suspend",
                                    (?THRESHOLD+1), "QueueFun", "QueueSize"),
-    verify_test_results(run_test(Nodes, BKV), ConsistentType, ProcFun2, QueueFun2),
+    verify_test_results(run_test(Nodes, BKV), BKV, ProcFun2, QueueFun2),
     Pid ! resume,
     ok.
 
 %% Don't check on fast path
-test_fsm_protection(_, {{<<"write_once_type">>, _}, _, _}, _) ->
+test_fsm_protection(_, ?WRITE_ONCE_BKV) ->
     ok;
 %% Or consistent gets, as they don't use the FSM either
-test_fsm_protection(_, _, true) ->
+test_fsm_protection(_, ?CONSISTENT_BKV) ->
     ok;
-test_fsm_protection(Nodes, BKV, ConsistentType) ->
+test_fsm_protection(Nodes, BKV) ->
     lager:info("Testing with coordinator protection enabled"),
     lager:info("Setting FSM limit to ~b", [?THRESHOLD]),
     %% Set FSM limit and reset other changes from previous tests.
@@ -170,12 +179,9 @@ test_fsm_protection(Nodes, BKV, ConsistentType) ->
             end, Nodes),
     Node1 = hd(Nodes),
 
-    %% TODO: Figure out why just using rt:wait_for_service completely breaks this test,
-    %% but not waiting for riak_kv leaves us open to a race where the resource doesn't exist yet.
-    %% Do the retry dance instead for now inside get_calculated_sj_limit.
     rt:wait_for_cluster_service(Nodes, riak_kv),
     rt:load_modules_on_nodes([?MODULE], Nodes),
-    {ok, ExpectedFsms} = get_calculated_sj_limit(Node1, riak_kv_get_fsm_sj),
+    {ok, ExpectedFsms} = get_calculated_sj_limit(Node1, riak_kv_get_fsm_sj, 1),
 
     %% We expect exactly ExpectedFsms, but because of a race in SideJob we sometimes get 1 more
     %% Adding 2 (the highest observed rasce to date) to the lte predicate to handle the occasional case.
@@ -184,7 +190,7 @@ test_fsm_protection(Nodes, BKV, ConsistentType) ->
                                  "ProcFun", "Procs"),
     QueueFun = build_predicate_lt(test_fsm_protection, (?NUM_REQUESTS),
                                   "QueueFun", "QueueSize"),
-    verify_test_results(run_test(Nodes, BKV), ConsistentType, ProcFun, QueueFun),
+    verify_test_results(run_test(Nodes, BKV), BKV, ProcFun, QueueFun),
 
     ok.
 
@@ -205,9 +211,7 @@ get_calculated_sj_limit(Node, ResourceName, Retries) when Retries > 0 ->
 get_calculated_sj_limit(Node, ResourceName, Retries) when Retries == 0 ->
     {error, io_lib:format("Failed to retrieve sidejob limit from ~p for resource ~p. Giving up.", [Node, ResourceName])}.
 
-test_cover_queries_overload(_Nodes, _, true) ->
-    ok;
-test_cover_queries_overload(Nodes, _, false) ->
+test_cover_queries_overload(Nodes) ->
     lager:info("Testing cover queries with vnode queue protection enabled"),
     lager:info("Setting vnode overload threshold to ~b", [?THRESHOLD]),
     lager:info("Setting vnode check interval to 1"),
