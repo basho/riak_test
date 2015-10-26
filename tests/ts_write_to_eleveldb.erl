12edb4b29786c6385c9229a852f14f82b9f4c353
Micah Warren <mwarren@basho.com>
2015-10-26 16:24:29 -0600

    First pass test, doesn't yet check order.
    This is just testing getting the intercepter in place, and filtering
    down to a single vnode.

---
+++ tests/ts_write_to_eleveldb.erl
@@ -0,0 +1,78 @@
+-module(ts_write_to_eleveldb).
+-compile({parse_transform, rt_intercept_pt}).
+-behavior(riak_test).
+-export([confirm/0]).
+-include_lib("eunit/include/eunit.hrl").
+
+-define(BUCKET, <<"GeoCheckin">>).
+
+confirm() ->
+
+    lager:info("Building cluster"),
+    [Node | _] = rt:deploy_nodes(_ClusterSize = 1, _Config = []),
+
+    lager:info("Creating and activating bucket"),
+    DDL = timeseries_util:get_ddl(docs),
+    {ok, _} = timeseries_util:create_bucket(Node, DDL),
+    _ = timeseries_util:activate_bucket(Node, DDL),
+
+    lager:info("installing interceoptor"),
+    Self = self(),
+
+    %% Based on experimentation, the path used for puts in ts is async.
+    ok = rt_intercept:add(Node, {riak_kv_eleveldb_backend, [
+        {{async_put, 5}, {[Self], fun(Context, Bucket, PrimaryKey, Val, State) ->
+            DoingPid = self(),
+            Self ! {reporting, DoingPid, {Bucket, PrimaryKey, Val}},
+            riak_kv_eleveldb_backend_orig:async_put_orig(Context, Bucket, PrimaryKey, Val, State)
+        end}}
+    ]}),
+
+    C = rt:pbc(Node),
+
+    lager:info("putting the datas"),
+    Data = make_data(),
+    ok = riakc_ts:put(C, ?BUCKET, Data),
+
+    %lager:info("test pulling the data"),
+    %{_Cols, TupleData} = riakc_ts:query(C,
+        %"select * from GeoCheckin"
+        %" where myfamily = 'family1'"
+        %" and myseries = 'series1'"
+        %" and time > 0"
+        %" and time < 100"),
+%
+    %?assertEqual(Data, lists:map(fun erlang:tuple_to_list/1, TupleData)),
+%
+    Intercepted = receive_intercepts(),
+    lager:info("Data Intercepted:"),
+    ok = lists:foreach(fun(I) ->
+        lager:info("        ~p", [I])
+    end, Intercepted),
+
+    lager:info("Data put: ~p", [Data]),
+
+    ?assertEqual(length(Data), length(Intercepted)),
+
+    pass.
+
+receive_intercepts() ->
+    receive_intercepts([], undefined).
+
+receive_intercepts(Acc, FilterToPid) ->
+    receive
+        {reporting, MaybePid, Thang} when FilterToPid == undefined orelse MaybePid =:= FilterToPid ->
+            receive_intercepts([Thang | Acc], MaybePid);
+        {reporting, _, _} ->
+            receive_intercepts(Acc, FilterToPid)
+    after
+        10000 ->
+            lists:reverse(Acc)
+    end.
+
+-define(DATA_POINTS, 10).
+make_data() ->
+    TimePoints = lists:seq(1, ?DATA_POINTS),
+    lists:map(fun(T) ->
+        [<<"family1">>, <<"series1">>, T, <<"weather-like">>, T / 2]
+    end, TimePoints).
