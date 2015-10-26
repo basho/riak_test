0bf182bbfb96e73ad22b989f8882bbaad5284d64
Mikael Lixenstrand <mikael.lixenstrand@erlang-solutions.com>
2015-10-26 23:49:45 +0100

    Add test for enable/disable

--- tests/verify_sweep_reaper.erl
+++ tests/verify_sweep_reaper.erl
@@ -27,7 +27,6 @@
 
 -module(verify_sweep_reaper).
 -behavior(riak_test).
--compile([export_all]).
 -export([confirm/0]).
 
 -include_lib("eunit/include/eunit.hrl").
@@ -36,6 +35,7 @@
 -define(NUM_KEYS, 1000).
 -define(BUCKET, <<"test_bucket">>).
 -define(N_VAL, 3).
+-define(WAIT_FOR_SWEEP, 15000).
 
 confirm() ->
     Config = [{riak_core, 
@@ -43,30 +43,76 @@ confirm() ->
                ]},
               {riak_kv,
                [{delete_mode, keep},
-                {reap_sweep_interval, 20},
+                {reap_sweep_interval, 5},
                 {sweep_tick, 1000},       %% Speed up sweeping
-                {anti_entropy, {off, []}} %% No AAE please
+                {storage_backend, riak_kv_memory_backend},
+                {anti_entropy, {off, []}}
                ]}
              ],
 
     Nodes = rt:build_cluster(1, Config),
-
-    [Client] =  create_pb_clients(Nodes),
-
-    %% Write key.
-    Key1 = <<"1">>,
-    write_key(Client, Key1, <<"one">>),
-    {fail,false} = delete_key(Client, Key1, 10, 6000),
+    [Client] = create_pb_clients(Nodes),
+    KV1 = test_data(1, 1000),
+    write_data(Client, KV1),
+    delete_keys(Client, KV1),
+    wait_for_sweep(),
     
-
+    false = check_reaps(Client, KV1),
+    disable_sweep_scheduling(Nodes),
     set_tombstone_grace(Nodes, 5),
-    %% Write key.
-    Key2 = <<"2">>,
-    write_key(Client, Key2, <<"two">>),
-    ok = delete_key(Client, Key2, 10, 6000),
-    ok = delete_key(Client, Key1, 10, 6000),
+    false = check_reaps(Client, KV1),
+    enable_sweep_scheduling(Nodes),
 
+    %% Write key.
+    KV2 = test_data(1001, 2000),
+    write_data(Client, KV2),
+    delete_keys(Client, KV2),
+    wait_for_sweep(),
+    true = check_reaps(Client, KV2),
+    true = check_reaps(Client, KV1),
     pass.
+
+wait_for_sweep() ->
+    lager:info("Wait for sweep ~p s", [?WAIT_FOR_SWEEP]),
+    timer:sleep(?WAIT_FOR_SWEEP).
+
+write_data(Client, KVs) ->
+    lager:info("Writing data ~p keys", [length(KVs)]),
+    write_data(Client, KVs, []).
+write_data(Client, KVs, Opts) ->
+    [begin
+         O = riakc_obj:new(?BUCKET, K, V),
+         ?assertMatch(ok, riakc_pb_socket:put(Client, O, Opts))
+     end || {K, V} <- KVs],
+    ok.
+test_data(Start, End) ->
+    Keys = [to_key(N) || N <- lists:seq(Start, End)],
+    [{K, K} || K <- Keys].
+
+to_key(N) ->
+    list_to_binary(io_lib:format("K~4..0B", [N])).
+
+delete_keys(Client, KVs) ->
+    lager:info("Delete data ~p keys", [length(KVs)]),
+    [{delete_key(Client, K)}  || {K, _V} <- KVs].
+delete_key(Client, Key) ->
+    riakc_pb_socket:delete(Client, ?BUCKET, Key).
+
+
+check_reaps(Client, KVs) ->
+    lager:info("Check data ~p keys", [length(KVs)]),
+    Results = [check_reap(Client, K)|| {K, _V} <- KVs],
+    Reaped = length([ true || true <- Results]),
+    lager:info("Reaped ~p", [Reaped]),
+    Reaped == length(KVs).
+
+check_reap(Client, Key) ->
+    case riakc_pb_socket:get(Client, ?BUCKET, Key, [deletedvclock]) of
+        {error, notfound, _} ->
+            false;
+        {error, notfound} ->
+            true
+    end.
 
 %%% Client/Key ops
 create_pb_clients(Nodes) ->
@@ -76,45 +122,14 @@ create_pb_clients(Nodes) ->
          C
      end || N <- Nodes].
 
-coordinating_client(Clients, Preflist) ->
-    {{_, FirstPrimary}, primary} = lists:keyfind(primary, 2, Preflist),
-    lists:keyfind(FirstPrimary, 1, Clients).
-
-up_client(DeadNode, Clients) ->
-    {value, _, LiveClients} = lists:keytake(DeadNode, 1, Clients),
-    hd(LiveClients).
-
-write_key(Client, Key, Val) ->
-    write_key(Client, Key, Val, []).
-
-write_key(Client, Key, Val, Opts) when is_binary(Val) ->
-    Object = case riakc_pb_socket:get(Client, ?BUCKET, Key, []) of
-                 {ok, O1} ->
-                     lager:info("writing existing!"),
-                     riakc_obj:update_value(O1, Val);
-                 _ ->
-                     lager:info("writing new!"),
-                     riakc_obj:new(?BUCKET, Key, Val)
-             end,
-    riakc_pb_socket:put(Client, Object, Opts).
-
-read_key(Client, Key) ->
-    riakc_pb_socket:get(Client, ?BUCKET, Key, []).
-
-delete_key(Client, Key, Retry, Delay) ->
-    riakc_pb_socket:delete(Client, ?BUCKET, Key),
-    Fun = 
-        fun() ->
-                case  riakc_pb_socket:get(Client, ?BUCKET, Key, [deletedvclock]) of
-                    {error, notfound, _} ->
-                        lager:info("Not reaped yet", []),
-                        false;
-                    {error, notfound} ->
-                        lager:info("Reaped", []),
-                        true
-                end
-        end,
-    rt:wait_until(Fun, Retry, Delay).
-
 set_tombstone_grace(Nodes, Time) ->
+    lager:info("set_tombstone_grace ~p s ", [Time]),
     rpc:multicall(Nodes, application, set_env, [riak_kv, tombstone_grace_period,Time]).
+
+disable_sweep_scheduling(Nodes) ->
+    rpc:multicall(Nodes, riak_kv_sweeper, disable_sweep_scheduling, []).
+
+enable_sweep_scheduling(Nodes) ->
+    rpc:multicall(Nodes, riak_kv_sweeper, enable_sweep_scheduling, []).
+    
+
