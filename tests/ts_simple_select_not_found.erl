dbad1b3c864a07a8e21c1c32f03b3ad6acdc42ad
Brett Hazen <brett@basho.com>
2016-01-11 17:40:38 +0000

    Switch from ts_A, B, C to ts_simple, cluster, degraded

---
+++ tests/ts_simple_select_not_found.erl
@@ -0,0 +1,48 @@
+%% -------------------------------------------------------------------
+%%
+%% Copyright (c) 2015 Basho Technologies, Inc.
+%%
+%% This file is provided to you under the Apache License,
+%% Version 2.0 (the "License"); you may not use this file
+%% except in compliance with the License.  You may obtain
+%% a copy of the License at
+%%
+%%   http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing,
+%% software distributed under the License is distributed on an
+%% "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+%% KIND, either express or implied.  See the License for the
+%% specific language governing permissions and limitations
+%% under the License.
+%%
+%% -------------------------------------------------------------------
+
+-module(ts_simple_select_not_found).
+
+-behavior(riak_test).
+
+-include_lib("eunit/include/eunit.hrl").
+
+-export([confirm/0]).
+
+%% Test selects over fields which are not in the
+%% primary key.
+
+confirm() ->
+    DDL = ts_util:get_ddl(),
+    Data = ts_util:get_valid_select_data(),
+    % weather is not part of the primary key, it is
+    % randomly generated data so this should return
+    % zero results
+    Qry =
+        "SELECT * FROM GeoCheckin "
+        "WHERE time > 1 AND time < 10 "
+        "AND myfamily = 'family1' "
+        "AND myseries = 'seriesX' "
+        "AND weather = 'summer rain'",
+    Expected = {[], []},
+    Got = ts_util:ts_query(
+            ts_util:cluster_and_connect(single), normal, DDL, Data, Qry),
+    ?assertEqual(Expected, Got),
+    pass.
