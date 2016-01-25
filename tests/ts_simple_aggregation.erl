801d56099d42c9d48d758c663618d0b4929562d4
andytill <atill@basho.com>
2016-01-25 10:02:55 +0000

    Rename STDDEV to STDDEV_POP.

--- tests/ts_simple_aggregation.erl
+++ tests/ts_simple_aggregation.erl
@@ -132,9 +132,9 @@ verify_aggregation(ClusterType) ->
     StdDevFun5 = stddev_fun_builder(Avg5),
     StdDev4 = math:sqrt(lists:foldl(StdDevFun4, 0, C4) / Count4),
     StdDev5 = math:sqrt(lists:foldl(StdDevFun5, 0, C5) / Count5),
-    Qry9 = "SELECT STDDEV(temperature), STDDEV(pressure) FROM " ++ Bucket ++ Where,
+    Qry9 = "SELECT STDDEV_POP(temperature), STDDEV_POP(pressure) FROM " ++ Bucket ++ Where,
     Got9 = ts_util:single_query(Conn, Qry9),
-    Expected9 = {[<<"STDDEV(temperature)">>, <<"STDDEV(pressure)">>],
+    Expected9 = {[<<"STDDEV_POP(temperature)">>, <<"STDDEV_POP(pressure)">>],
                  [{StdDev4, StdDev5}]},
     Result9 = ts_util:assert_float(test_name(ClusterType, "Standard Deviation"), Expected9, Got9),
 
