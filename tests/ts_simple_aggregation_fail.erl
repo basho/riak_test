dc8668ca17def11018a6af28cf347ab63b8f045a
Brett Hazen <brett@basho.com>
2016-02-02 21:20:56 +0000

    Remove arity from SQL aggregate window functions

--- tests/ts_simple_aggregation_fail.erl
+++ tests/ts_simple_aggregation_fail.erl
@@ -36,42 +36,42 @@ confirm() ->
     Qry = "SELECT SUM(mybool) FROM " ++ Bucket,
     ClusterConn = {_Cluster, Conn} = ts_util:cluster_and_connect(single),
     Got1 = ts_util:ts_query(ClusterConn, TestType, DDL, Data, Qry, Bucket),
-    Expected1 = {error, {1001, <<"invalid_query: \nFunction 'SUM'/1 called with arguments of the wrong type [boolean].">>}},
+    Expected1 = {error, {1001, <<"invalid_query: \nFunction 'SUM' called with arguments of the wrong type [boolean].">>}},
     Result1 = ts_util:assert("SUM - boolean", Expected1, Got1),
 
     Qry2 = "SELECT AVG(myfamily) FROM " ++ Bucket,
     Got2 = ts_util:single_query(Conn, Qry2),
-    Expected2 = {error, {1001, <<"invalid_query: \nFunction 'AVG'/1 called with arguments of the wrong type [varchar].">>}},
+    Expected2 = {error, {1001, <<"invalid_query: \nFunction 'AVG' called with arguments of the wrong type [varchar].">>}},
     Result2 = ts_util:assert("AVG - varchar", Expected2, Got2),
 
     Qry3 = "SELECT MIN(myseries) FROM " ++ Bucket,
     Got3 = ts_util:single_query(Conn, Qry3),
-    Expected3 = {error, {1001, <<"invalid_query: \nFunction 'MIN'/1 called with arguments of the wrong type [varchar].">>}},
+    Expected3 = {error, {1001, <<"invalid_query: \nFunction 'MIN' called with arguments of the wrong type [varchar].">>}},
     Result3 = ts_util:assert("MIN - varchar", Expected3, Got3),
 
     Qry4 = "SELECT MAX(myseries) FROM " ++ Bucket,
     Got4 = ts_util:single_query(Conn, Qry4),
-    Expected4 = {error, {1001, <<"invalid_query: \nFunction 'MAX'/1 called with arguments of the wrong type [varchar].">>}},
+    Expected4 = {error, {1001, <<"invalid_query: \nFunction 'MAX' called with arguments of the wrong type [varchar].">>}},
     Result4 = ts_util:assert("MIN - varchar", Expected4, Got4),
 
     Qry5 = "SELECT STDDEV(mybool) FROM " ++ Bucket,
     Got5 = ts_util:single_query(Conn, Qry5),
-    Expected5 = {error, {1001, <<"invalid_query: \nFunction 'STDDEV_SAMP'/1 called with arguments of the wrong type [boolean].">>}},
+    Expected5 = {error, {1001, <<"invalid_query: \nFunction 'STDDEV_SAMP' called with arguments of the wrong type [boolean].">>}},
     Result5 = ts_util:assert("STDDEV - boolean", Expected5, Got5),
 
     Qry6 = "SELECT STDDEV_SAMP(mybool) FROM " ++ Bucket,
     Got6 = ts_util:single_query(Conn, Qry6),
-    Expected6 = {error, {1001, <<"invalid_query: \nFunction 'STDDEV_SAMP'/1 called with arguments of the wrong type [boolean].">>}},
+    Expected6 = {error, {1001, <<"invalid_query: \nFunction 'STDDEV_SAMP' called with arguments of the wrong type [boolean].">>}},
     Result6 = ts_util:assert("STDDEV_SAMP - boolean", Expected6, Got6),
 
     Qry7 = "SELECT STDDEV_POP(time) FROM " ++ Bucket,
     Got7 = ts_util:single_query(Conn, Qry7),
-    Expected7 = {error, {1001, <<"invalid_query: \nFunction 'STDDEV_POP'/1 called with arguments of the wrong type [timestamp].">>}},
+    Expected7 = {error, {1001, <<"invalid_query: \nFunction 'STDDEV_POP' called with arguments of the wrong type [timestamp].">>}},
     Result7 = ts_util:assert("STDDEV_POP - timestamp", Expected7, Got7),
 
     Qry8 = "SELECT Mean(mybool) FROM " ++ Bucket,
     Got8 = ts_util:single_query(Conn, Qry8),
-    Expected8 = {error, {1001, <<"invalid_query: \nFunction 'AVG'/1 called with arguments of the wrong type [boolean].">>}},
+    Expected8 = {error, {1001, <<"invalid_query: \nFunction 'AVG' called with arguments of the wrong type [boolean].">>}},
     Result8 = ts_util:assert("MEAN - boolean", Expected8, Got8),
 
     ts_util:results([
