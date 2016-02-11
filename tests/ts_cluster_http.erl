5ff9b8bf009a1f8c3a5639aba8744156719bff09
Andrei Zavada <johnhommer@gmail.com>
2016-02-11 14:51:33 +0200

    test TS HTTP API, reusing ts_cluster_comprehensive scenario
    The ts_cluster_comprehensive is extended to transparently use either
    rpb or rhc modules (riakc and riakhttpc, resp.); pbc will be used by
    default. To use the HTTP client, run ./riak_test -t ts_cluster_http.

---
+++ tests/ts_cluster_http.erl
@@ -0,0 +1,31 @@
+%% -------------------------------------------------------------------
+%%
+%% Copyright (c) 2016 Basho Technologies, Inc.
+%%
+%% This file is provided to you under the Apache License,
+%% Version 2.0 (the "License"); you may not use this file
+%% except in compliance with the License.  You may obtain
+%% a copy of the License at
+%%
+%%   http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing,
+%% software distributed under the License is distributed on an
+%% "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+%% KIND, either express or implied.  See the License for the
+%% specific language governing permissions and limitations
+%% under the License.
+%%
+%% -------------------------------------------------------------------
+%% @doc A module to test riak_ts basic create bucket/put/select cycle
+%%      using http client (rhc_ts).
+
+-module(ts_cluster_http).
+-behavior(riak_test).
+-export([confirm/0]).
+
+-define(PVAL_P1, <<"ZXC11">>).
+-define(PVAL_P2, <<"PDP-11">>).
+
+confirm() ->
+    ts_cluster_comprehensive:run_tests(?PVAL_P1, ?PVAL_P2, rhc_ts).
