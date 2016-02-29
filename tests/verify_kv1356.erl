d727c9eed68578440c3b66b50b2f9f86561e82f8
Russell Brown <russell.brown@mac.com>
2016-02-29 17:09:26 +0000

    Add basic test for riak_kv#1356
    Start riak, add data, stop riak, read data. With known bad level
    config.

---
+++ tests/verify_kv1356.erl
@@ -0,0 +1,67 @@
+%% -------------------------------------------------------------------
+%%
+%% Copyright (c) 2012 Basho Technologies, Inc.
+%%
+%% This file is provided to you under the Apache License,
+%% Version 2.0 (the "License"); you may not use this file
+%% except in compliance with the License.  You may obtain
+%% a copy of the License at
+%%
+%%   http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing,
+%% software distributed under the License is distributed on an
+%% "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+%% KIND, either express or implied.  See the License for the
+%% specific language governing permissions and limitations
+%% under the License.
+%%
+%% -------------------------------------------------------------------
+
+%%% @doc This test was written after
+%%% https://github.com/basho/riak_kv/issues/1356 was opened. The user
+%%% reported, under certain config, that riak simply "forgets"
+%%% data. This is the bare minimum for a database. This test verifies
+%%% that if we write a key/value we can read it. If we stop and start
+%%% the node we can read it. If we kill and start the node we can read
+%%% it.
+%%% @end
+
+
+-module(verify_kv1356).
+-behavior(riak_test).
+-export([confirm/0]).
+
+-include_lib("eunit/include/eunit.hrl").
+
+-define(BUCKET, <<"b">>).
+-define(KEY, <<"k">>).
+-define(VALUE, <<"v">>).
+
+confirm() ->
+    Conf = [{eleveldb, [{data_root, "leveldb"},
+                        {tiered_slow_level, 4},
+                        {tiered_fast_prefix, "/opt/riak/fast/"},
+                        {tiered_slow_prefix, "/opt/riak/slow/"}
+                       ]},
+            {riak_kv, [{handoff_concurrency, 100},
+                       {storage_backend, riak_kv_eleveldb_backend}]},
+            {riak_core, [ {ring_creation_size, 8},
+                          {vnode_management_timer, 1000}]}],
+    lager:info("Deploy a node"),
+    [Node] = rt:deploy_nodes(1, Conf),
+    Client = rt:pbc(Node, [{auto_reconnect, true}, {queue_if_disconnected, true}]),
+    lager:info("write value"),
+    ok = rt:pbc_write(Client, ?BUCKET, ?KEY, ?VALUE),
+    lager:info("read it back"),
+    assertObjectValueEqual(?VALUE, rt:pbc_read(Client, ?BUCKET, ?KEY)),
+    lager:info("Stop the node"),
+    rt:stop_and_wait(Node),
+    lager:info("Start the node"),
+    rt:start_and_wait(Node),
+    lager:info("read the value"),
+    assertObjectValueEqual(?VALUE, rt:pbc_read(Client, ?BUCKET, ?KEY)),
+    pass.
+
+assertObjectValueEqual(Val, Obj) ->
+    ?assertEqual(Val, riakc_obj:get_value(Obj)).
