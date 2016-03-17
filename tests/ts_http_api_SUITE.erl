2374f2ed84373b5bfced69200df83bf968749ac3
Torben Hoffmann <thoffmann@basho.com>
2016-03-17 08:48:23 +0100

    HTTP API: create_bad_table_test passes

--- tests/ts_http_api_SUITE.erl
+++ tests/ts_http_api_SUITE.erl
@@ -56,7 +56,8 @@ groups() ->
     [].
 
 all() ->
-    [ create_table_test
+    [ create_table_test,
+      create_bad_table_test
     ].
 
 
@@ -72,7 +73,11 @@ table_def_bob() ->
         " d sint64," ++
         " primary key ((a, b, quantum(c, 1, m)), a, b, c))".
 
-
+bad_table_def() ->
+    "create table pap ("++
+        "a timestamp not null," ++
+        "b timestamp not null," ++
+        "c timestamp not null)".
 
 %% client_pid(Ctx) ->
 %%     [Node|_] = proplists:get_value(cluster, Ctx),
@@ -84,16 +89,34 @@ table_def_bob() ->
 
 create_table_test(Cfg) ->
     Query = table_def_bob(),
-    [Node|_] = ?config(cluster, Cfg),
-    {IP, Port} = node_ip_and_port(Node),
-    URL = query_url(IP, Port, Query),
+    Node = get_node(Cfg),
+    URL = query_url(Node, Query),
     ct:log("URL=~s", [URL]),
     {ok, "200", _Headers, Body } = ibrowse:send_req(URL, [], post),
     Body = success_body().
 
+
+
+create_bad_table_test(Cfg) ->
+    Query = bad_table_def(),
+    Node = get_node(Cfg),
+    URL = query_url(Node, Query),
+    {ok, "400", _Headers, "bad query:"++_} = ibrowse:send_req(URL, [], post).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% Helper functions
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+get_node(Cfg) ->
+    [Node|_] = ?config(cluster, Cfg),
+    Node.
+
 node_ip_and_port(Node) ->
     {ok, [{IP, Port}]} = rpc:call(Node, application, get_env, [riak_api, http]),
     {IP, Port}.
+
+query_url(Node, Query) ->
+     {IP, Port} = node_ip_and_port(Node),
+     query_url(IP, Port, Query).
 
 query_url(IP, Port, Query) ->
     EncodedQuery = ibrowse_lib:url_encode(Query),
