34e7a593db85700b7b013b289577e45c63875b50
erikleitch <eleitch@basho.com>
2016-03-23 10:40:03 -0700

    Added scripts/bashfns

--- tests/ts_query.erl
+++ tests/ts_query.erl
@@ -5,17 +5,15 @@
 -include_lib("profiler/include/profiler.hrl").
 
 confirm() ->
-    perf_profile({prefix, "./profiler_results"}),
+  profiler:profile({prefix, "./client_profiler_results"}),
 
-    DDL  = ts_api_util:get_ddl(api),
-    UseNativeEncoding=true,
-    Data = [[<<"family1">>, <<"seriesX">>, 100, 1, <<"test1">>, 1.0, true]],
-    ts_api_util:setup_cluster_for_single_query(single, n_val_one, DDL, true, Data, UseNativeEncoding),
+  DDL  = ts_api_util:get_ddl(api),
+  UseNativeEncoding=true,
+  ts_api_util:setup_cluster_put_mod_time(single, n_val_one, DDL, 1, true, UseNativeEncoding),
+  Query = "SELECT * FROM GeoCheckin WHERE time >= 0 AND time <= 1 AND myfamily = 'family1' AND myseries = 'seriesX'",
 
-    Query = "SELECT * FROM GeoCheckin WHERE time >= 0 AND time < 1000 AND myfamily = 'family1' AND myseries = 'seriesX'",
+  profiler:profile({start, 0}),
+  ts_api_util:runQuery(Query, 1),
+  profiler:profile({stop, 0}),
 
-    profiler:perf_profile({start, 0}),
-    ts_api_util:runQuery(Query, 1000),
-    profiler:perf_profile({stop, 0}),
-
-    pass.
+  pass.
