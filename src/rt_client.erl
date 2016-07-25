6bc967b0e00308dfdf2beac333f58370a3ac4343
Andrei Zavada <johnhommer@gmail.com>
2016-07-25 19:58:56 +0300

    rt_client: remove unused code
    no code swapping for clients

--- src/rt_client.erl
+++ src/rt_client.erl
@@ -21,7 +21,6 @@
 
 -export([
          client_vsn/0,
-         load/1,
          set_up_slave_for_previous_client/1
         ]).
 
@@ -42,59 +41,3 @@ set_up_slave_for_previous_client(SlaveNode) ->
     true = rpc:call(
              SlaveNode, code, replace_path, ["riakc", PrevRiakcPath]),
     SlaveNode.
-
-load(Version) ->
-    LoadPaths = get_load_paths(Version),
-    Modules = get_modules(LoadPaths, []),
-    %% ct:pal("switching client from vsn ~s (~s)", [current_client_vsn(), Version]),
-    ok = unload_riak_client(Modules),
-    ok = load_riak_client(Modules),
-    %% ct:pal("switched client to vsn ~s", [current_client_vsn()]),
-    ok.
-
-load_riak_client(Modules) ->
-    [{module, Mod} = code:load_abs(File) || {File, Mod} <- Modules],
-    {ok, _} = application:ensure_all_started(riakc),
-    ok.
-
-get_modules([], Acc) ->
-    lists:flatten(Acc);
-get_modules([H | T], Acc) ->
-    Files = filelib:wildcard(filename:join([H, "*.beam"])),
-    Files2 = [filename:rootname(File) || File <- Files],
-    Mods = extract_mods(Files),
-    Zip = lists:zip(Files2, Mods),
-    get_modules(T, [Zip | Acc]).
-
-extract_mods(Mods) ->
-    [list_to_atom(filename:rootname(filename:basename(Mod))) || Mod <- Mods].
-
-get_load_paths(Version) ->
-    Root = rtdev:relpath(Version),
-    [
-     filename:join([Root, "dev/dev1/lib/riakc*/ebin"]),
-     filename:join([Root, "dev/dev1/lib/riak_pb*/ebin"])
-    ].
-
-%% we have a problem: the set of files in Version X of the client
-%% might not be congruent with that of Version Y
-%% so we will hard unload files from the app manifest
-%% and then iterate over the beam files in the path and unload any
-%% modules with those names - all a bit brute-force-and-ignorance
-unload_riak_client(Modules) ->
-    %% app first
-    _ = application:stop(riak_pb),
-    _ = application:stop(riakc),
-    [ok = unload(App) || App <- [riakc, riak_pb]],
-    [begin code:purge(Mod), code:delete(Mod) end || {_File, Mod} <- Modules],
-    ok.
-
-unload(App) ->
-    _ = application:load(App),
-    case application:get_key(App, modules) of
-        {ok, Modules} ->
-            [begin code:purge(Mod), code:delete(Mod) end || Mod <- Modules],
-            ok;
-        _Other ->
-            ok
-    end.
