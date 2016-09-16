e771fee2d88967bc98f80fd8c13569f0a3f5c38a
Brian Sparrow <bsparrow435@gmail.com>
2016-09-16 18:33:22 -0400

    Add support for AAE hashing version 0 (#1148)
    * Add support for AAE hashing version 0
    
    Additionally, change verify_aae to use 3 nodes for more complete
    testing.
    
    * Update repl_aae_fullsync.erl
    
    * Update verify_aae.erl
    
    * Do hashtree upgrade verification first
    
    * Fix intercepts in aae_fullsync test
    
    * Increase node count for more accurate test

--- tests/verify_aae.erl
+++ tests/verify_aae.erl
@@ -60,7 +60,7 @@
            {ring_creation_size, ?DEFAULT_RING_SIZE}
           ]}]
        ).
--define(NUM_NODES, 1).
+-define(NUM_NODES, 3).
 -define(NUM_KEYS, 1000).
 -define(BUCKET, <<"test_bucket">>).
 -define(N_VAL, 3).
@@ -88,7 +88,11 @@ verify_throttle_config(Nodes) ->
 
 verify_aae(Nodes) ->
     Node1 = hd(Nodes),
-    % First, recovery without tree rebuilds
+
+    % Verify that AAE eventually upgrades to version 0(or already has)
+    wait_until_hashtree_upgrade(Nodes), 
+    
+    % Recovery without tree rebuilds
 
     % Test recovery from to few replicas written
     KV1 = test_data(1, 1000),
@@ -101,7 +105,7 @@ verify_aae(Nodes) ->
     lager:info("Run similar tests now with tree rebuilds enabled"),
     start_tree_rebuilds(Nodes),
 
-    % Test recovery from to few replicas written
+    % Test recovery from too few replicas written
     KV3 = test_data(1001, 2000),
     test_less_than_n_writes(Node1, KV3),
 
@@ -320,3 +324,26 @@ max_aae_repairs(Node) when is_atom(Node) ->
     LastCounts = [Last || {_, _, _, {Last, _, _, _}} <- Info],
     MaxCount = lists:max(LastCounts),
     MaxCount.
+
+wait_until_hashtree_upgrade(Nodes) ->
+    lager:info("Verifying AAE hashtrees eventually all upgrade to version 0"),
+    rt:wait_until(fun() -> all_hashtrees_upgraded(Nodes) end).
+
+all_hashtrees_upgraded(Nodes) when is_list(Nodes) ->
+    [Check|_] = lists:usort([all_hashtrees_upgraded(Node) || Node <- Nodes]),
+    Check;
+
+all_hashtrees_upgraded(Node) when is_atom(Node) ->
+    case rpc:call(Node, riak_kv_entropy_manager, get_version, []) of
+        0 ->
+            Trees = rpc:call(Node, riak_kv_entropy_manager, get_trees_version, []),
+            case [Idx || {Idx, undefined} <- Trees] of
+                [] ->
+                    true;
+                _ ->
+                    false
+            end;
+        _ ->
+            false
+     end.
+    
