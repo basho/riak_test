ab98d4f8e4d247bf6715bfd74af314a33835cdfd
Russell Brown <russell.brown@mac.com>
2016-10-21 15:00:13 +0100

    Test the range of range queries

--- tests/bigset_range_query.erl
+++ tests/bigset_range_query.erl
@@ -35,27 +35,58 @@ confirm() ->
 
     %% range start - range end
     {ok, _, {_, Res}} = bigset_client:read(?SET, [{range_start, make_element(30)},
-                                                  {range_end, make_element(50)},
-                                                  {end_inclusive, true}],
+                                                  {range_end, make_element(50)}],
                                            N2Client),
 
     assertRange({30, 50}, Res),
-    %% set start - range end
-    %% range start - set end
-    %% range start - range end (where range end > set end)
-    %% range start - range end (where start < set start)
-    %% range start > range end
 
+    {ok, _, {_, Res2}} = bigset_client:read(?SET, [{range_start, make_element(30)},
+                                                   {range_end, make_element(50)},
+                                                   {end_inclusive, false},
+                                                   {start_inclusive, false}],
+                                            N2Client),
+
+    assertRange({31, 49}, Res2),
+
+    %% set start - range end
+    {ok, _, {_, Res3}} = bigset_client:read(?SET, [{range_end, make_element(50)},
+                                                   {end_inclusive, false}],
+                                            N2Client),
+
+    assertRange({1, 49}, Res3),
+    %% range start - set end
+    {ok, _, {_, Res4}} = bigset_client:read(?SET, [{range_start, make_element(50)},
+                                                   {start_inclusive, false}],
+                                            N2Client),
+
+    assertRange({51, 100}, Res4),
+    %% range start - range end (where range end > set end)
+    {ok, _, {_, Res4}} = bigset_client:read(?SET, [{range_start, make_element(50)},
+                                                   {start_inclusive, false},
+                                                   {range_end, make_element(10000)}],
+                                            N2Client),
+
+    assertRange({51, 100}, Res4),
+    %% range start - range end (where start < set start)
+    {ok, _, {_, Res5}} = bigset_client:read(?SET, [{range_start, make_element(0)},
+                                                   {range_end, make_element(10)}],
+                                            N2Client),
+
+    assertRange({1, 10}, Res5),
+    %% range start > range end
+    try
+        NotErr = bigset_client:read(?SET, [{range_start, make_element(50)},
+                                           {range_end, make_element(1)}],
+                                    N2Client),
+        ?assertEqual("Expected an error", NotErr)
+    catch throw:E ->
+            ?assertMatch({error,invalid_options,[{invalid_range_options, _, _}], _}, E),
+            lager:info("Expected error: ~p", [E])
+    end,
     pass.
 
 make_element(I) when is_integer(I) ->
     <<I:32/big-unsigned-integer>>.
-
-%% assert_elem_present(Elem, ElemList) ->
-%%     ?assertMatch({_, _}, lists:keyfind(Elem, 1, ElemList)).
-
-%% assert_elem_absent(Elem, ElemList) ->
-%%     ?assertEqual(false,  lists:keyfind(Elem, 1, ElemList)).
 
 make_range(Start, Finish) ->
     [ make_element(I)  || I <- lists:seq(Start, Finish)].
