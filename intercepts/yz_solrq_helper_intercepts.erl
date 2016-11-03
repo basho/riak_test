cc6fc9f6c4714b7b034801dad9cef14fd378b505
Doug Rohrer <drohrer@basho.com>
2016-11-02 20:11:09 -0400

    Rename intercept to match updated code in yokozuna.

--- intercepts/yz_solrq_helper_intercepts.erl
+++ intercepts/yz_solrq_helper_intercepts.erl
@@ -24,10 +24,10 @@
 
 -define(M, yz_solrq_helper_orig).
 
-handle_get_ops_for_crdt_deletes(LI, P, Obj) ->
+handle_get_ops_for_no_sibling_deletes(LI, P, Obj) ->
     Lookup = ets:lookup(intercepts_tab, del_put),
     case Lookup of
-        [] -> original_get_ops_for_crdt_deletes(LI, P, Obj);
+        [] -> original_get_ops_for_no_sibling_deletes(LI, P, Obj);
         _ ->
             case proplists:get_value(del_put, Lookup) of
                 0 ->
@@ -37,12 +37,12 @@ handle_get_ops_for_crdt_deletes(LI, P, Obj) ->
                     ets:update_counter(intercepts_tab, del_put, 1),
                     [];
                 _ ->
-                    original_get_ops_for_crdt_deletes(LI, P, Obj)
+                    original_get_ops_for_no_sibling_deletes(LI, P, Obj)
             end
     end.
 
-original_get_ops_for_crdt_deletes(LI, P, Obj) ->
+original_get_ops_for_no_sibling_deletes(LI, P, Obj) ->
     error_logger:info_msg(
       "Delete operation original for BKey ~p",
         [{riak_object:bucket(Obj), riak_object:key(Obj)}]),
-    ?M:get_ops_for_crdt_deletes_orig(LI, P, Obj).
+    ?M:get_ops_for_no_sibling_deletes_orig(LI, P, Obj).
--- tests/yz_crdt.erl
+++ tests/yz_crdt.erl
@@ -256,8 +256,8 @@ test_and_validate_delete_aae(Pid, Cluster, Bucket, Index) ->
 
     [make_intercepts_tab(ANode) || ANode <- Cluster],
 
-    [rt_intercept:add(ANode, {yz_solrq_helper, [{{get_ops_for_crdt_deletes, 3},
-                                        handle_get_ops_for_crdt_deletes}]})
+    [rt_intercept:add(ANode, {yz_solrq_helper, [{{get_ops_for_no_sibling_deletes, 3},
+                                        handle_get_ops_for_no_sibling_deletes}]})
      || ANode <- Cluster],
     [true = rpc:call(ANode, ets, insert, [intercepts_tab, {del_put, 0}]) ||
         ANode <- Cluster],
