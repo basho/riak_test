b639252aca9d7210aaf3df1f0708cfa58fd1bda7
Paul Henrich + Jason Voegele <phenrich+ph+jv@basho.com>
2016-11-16 16:29:37 -0500

    Tests for granting and creating users

--- tests/s3_users.erl
+++ tests/s3_users.erl
@@ -9,23 +9,35 @@
 confirm() ->
     Cluster = [Node| _ ] = build_cluster(2),
     enable_security(Node),
-    %% verify_create_user(Cluster),
-    verify_upgrade_user(Cluster),
+    verify_create_user(Cluster),
+    verify_grant_user(Cluster),
     pass.
 
-verify_upgrade_user([Node|_]) ->
+verify_create_user([Node|_]) ->
     Username = "username",
+    DisplayName = "displayname",
+    User = rpc:call(Node,
+                           riak_s3_user,
+                           create,
+                           [Username, DisplayName]),
+    {Username, Options} = rpc:call(Node, riak_core_security, get_user, [Username]),
+
+    io:format("User: ~p", [User]),
+    io:format("Options: ~p", [Options]),
+    ?assertEqual(DisplayName, proplists:get_value("s3.DisplayName", Options)).
+
+verify_grant_user([Node|_]) ->
+    Username = "jorge",
     DisplayName = "displayname",
     ok = rpc:call(Node, riak_core_security, add_user, [Username, []]),
     {ok, {AccessKey, SecretKey}} = rpc:call(Node,
                                             riak_s3_user_riak_security_backend,
-                                            enable_s3_user, [Username, DisplayName]),
-    UsernameBinary = list_to_binary(Username),
-    {UsernameBinary, Options} = rpc:call(Node, riak_core_security, get_user, [Username]),
+                                            grant_s3_to_user, [Username, DisplayName]),
+    {Username, Options} = rpc:call(Node, riak_core_security, get_user, [Username]),
 
-    ?assertEqual(proplists:get_value("s3.DisplayName", Options), DisplayName),
-    ?assertEqual(proplists:get_value("s3.AccessKeyId", Options), AccessKey),
-    ?assertEqual(proplists:get_value("s3.SecretAccessKey", Options), SecretKey).
+    ?assertEqual(DisplayName, proplists:get_value("s3.DisplayName", Options)),
+    ?assertEqual(AccessKey, proplists:get_value("s3.AccessKeyId", Options)),
+    ?assertEqual(SecretKey, proplists:get_value("s3.SecretAccessKey", Options)).
 
 build_cluster(N) ->
     rt:build_cluster(lists:duplicate(N, {current, []})).
