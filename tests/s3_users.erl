c719b487e42351d41ea1d8718ab0c9feed7bb88f
Jason Voegele <jason.voegele@gmail.com>
2016-11-17 14:51:30 -0500

    Check that user propagates over nodes

--- tests/s3_users.erl
+++ tests/s3_users.erl
@@ -13,30 +13,38 @@ confirm() ->
     verify_grant_user(Cluster),
     pass.
 
-verify_create_user([Node|_]) ->
+verify_create_user([Node1, _Node2|_]) ->
     Username = "username",
     DisplayName = "displayname",
-    User = rpc:call(Node,
-                           riak_s3_user,
-                           create,
-                           [Username, DisplayName]),
-    DN = rpc:call(Node, riak_s3_user, get_display_name, [User]),
+    User = rpc:call(Node1,
+                    riak_s3_user,
+                    create,
+                    [Username, DisplayName]),
+    AccessKey = rpc:call(Node1, riak_s3_user, get_access_key_id, [User]),
+    io:format("User: ~p AccessKey: ~p~n", [User, AccessKey]),
+    rt:wait_until(
+      fun() ->
+              User2 = rpc:call(Node1, riak_s3_user, find_by_access_key, [AccessKey]),
+              % TODO: name vs username, pick one
+              Username =:= rpc:call(Node1, riak_s3_user, get_name, [User2]) andalso
+              DisplayName =:= rpc:call(Node1, riak_s3_user, get_display_name, [User2])
+      end).
 
-    io:format("User: ~p", [User]),
-    ?assertEqual(DisplayName, DN).
-
-verify_grant_user([Node|_]) ->
+verify_grant_user([Node1, Node2|_]) ->
     Username = "jorge",
     DisplayName = "displayname",
-    ok = rpc:call(Node, riak_core_security, add_user, [Username, []]),
-    {ok, {AccessKey, SecretKey}} = rpc:call(Node,
+    ok = rpc:call(Node1, riak_core_security, add_user, [Username, []]),
+    {ok, {AccessKey, SecretKey}} = rpc:call(Node1,
                                             riak_s3_user_riak_security_backend,
                                             grant_s3_to_user, [Username, DisplayName]),
-    {Username, Options} = rpc:call(Node, riak_core_security, get_user, [Username]),
+    rt:wait_until(
+      fun() ->
+              {Username, Options} = rpc:call(Node2, riak_core_security, get_user, [Username]),
 
-    ?assertEqual(DisplayName, proplists:get_value("s3.DisplayName", Options)),
-    ?assertEqual(AccessKey, proplists:get_value("s3.AccessKeyId", Options)),
-    ?assertEqual(SecretKey, proplists:get_value("s3.SecretAccessKey", Options)).
+              DisplayName =:= proplists:get_value("s3.DisplayName", Options) andalso
+              AccessKey =:= proplists:get_value("s3.AccessKeyId", Options) andalso
+              SecretKey =:= proplists:get_value("s3.SecretAccessKey", Options)
+      end).
 
 build_cluster(N) ->
     rt:build_cluster(lists:duplicate(N, {current, []})).
