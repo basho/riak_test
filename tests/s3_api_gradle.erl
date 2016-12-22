8f8f1912581b1f2b396fabda03a9f0d6ab03702f
Jon Brisbin <jon@jbrisbin.com>
2016-12-22 14:08:03 -0600

    Change to the riak_test to first build a proper cluster, then create a riak security user, wait for the user to exist throughout the cluster, then pass the information about access and secret keys to the gradle tests via the command line, all which eliminates the need for the profile backend in these tests.

--- tests/s3_api_gradle.erl
+++ tests/s3_api_gradle.erl
@@ -1,33 +1,54 @@
 -module(s3_api_gradle).
--include_lib("eunit/include/eunit.hrl").
 
 -behavior(riak_test).
+
+-include_lib("eunit/include/eunit.hrl").
+
 -export([
     confirm/0
 ]).
 
 -define(NODE_COUNT, 3).
+-define(TEST_USER, ["test", "test@example.com"]).
+-define(METADATA_KEY, [{<<"security">>, <<"users">>}, <<"test">>]).
 
 confirm() ->
-    Nodes = rt:deploy_nodes(?NODE_COUNT),
-    ok = rt:wait_until_nodes_ready(Nodes),
+    %% Build a simple cluster
+    Nodes = rt:build_cluster(?NODE_COUNT),
 
-    %% Set to testing profile backend on all the nodes
-    {[ok, ok, ok], []} = rpc:multicall(Nodes,
-                                       application,
-                                       set_env,
-                                       [riak_s3_api, default_user_backend, riak_s3_user_profile_backend]),
+    %% Choose random node to interact with
+    Node = lists:nth(random:uniform(?NODE_COUNT), Nodes),
+
+    %% Create test user and wait for it to appear on the cluster
+    _ = rpc:call(Node, riak_s3_user, create, ?TEST_USER),
+    rt:wait_until(fun() ->
+                        {Results, []} = rpc:multicall(Nodes, riak_core_metadata, get, ?METADATA_KEY),
+                        case lists:filter(fun(R) -> R == undefined end, Results) of
+                            [] -> false;
+                            _ -> true
+                        end
+                  end),
+    [
+        {"s3.access_key_id", S3AccessKey},
+        {"s3.display_name", _},
+        {"s3.secret_access_key", S3SecretKey}
+    ] = rpc:call(Node, riak_core_metadata, get, ?METADATA_KEY),
 
     %% Get S3 URLs
     URLs = rt:s3_url(Nodes),
     S3_URL = lists:nth(random:uniform(?NODE_COUNT), URLs),
 
     %% Run Gradle-based tests
-    Cmd = lists:flatten([
-                            "cd ../s3-api-tests",
-                            " && ",
-                            io_lib:format("./gradlew -Ds3.api.baseUrl=~s test --info", [S3_URL])
-                        ]),
+    Args = [
+        S3AccessKey,
+        S3SecretKey,
+        S3_URL
+    ],
+    Cmd = lists:flatten(io_lib:format(
+        "cd ../s3-api-tests"
+        " && "
+        "./gradlew -Daws.accessKeyId=~s -Daws.secretKey=~s -Ds3.api.baseUrl=~s test --info",
+        Args)),
     lager:info(Cmd),
     Output = os:cmd(Cmd),
     Lines0 = string:tokens(Output, "\n"),
@@ -36,8 +57,8 @@ confirm() ->
     [lager:info(O) || O <- Lines0],
 
     %% Check output for lines that end in 'FAILED'
-    Lines1 = lists:filter(fun ("DELIAF" ++ _) -> true;
-                              (_) -> false
+    Lines1 = lists:filter(fun("DELIAF" ++ _) -> true;
+                             (_) -> false
                           end,
                           [lists:reverse(L) || L <- Lines0]),
 
