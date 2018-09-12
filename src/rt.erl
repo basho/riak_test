439368d5845a41382efa5ab87e200764168e9242
Russell Brown <russell.brown@mac.com>
2018-09-12 15:34:53 +0100

    add leveled support

--- src/rt.erl
+++ src/rt.erl
@@ -1758,6 +1758,8 @@ set_backend(Backend) ->
     set_backend(Backend, []).
 
 -spec set_backend(atom(), [{atom(), term()}]) -> atom()|[atom()].
+set_backend(leveled, _) ->
+    set_backend(riak_kv_leveled_backend);		     
 set_backend(bitcask, _) ->
     set_backend(riak_kv_bitcask_backend);
 set_backend(eleveldb, _) ->
@@ -1766,7 +1768,10 @@ set_backend(memory, _) ->
     set_backend(riak_kv_memory_backend);
 set_backend(multi, Extras) ->
     set_backend(riak_kv_multi_backend, Extras);
-set_backend(Backend, _) when Backend == riak_kv_bitcask_backend; Backend == riak_kv_eleveldb_backend; Backend == riak_kv_memory_backend ->
+set_backend(Backend, _) when Backend == riak_kv_bitcask_backend;
+		             Backend == riak_kv_eleveldb_backend;
+			     Backend == riak_kv_memory_backend;
+			     Backend == riak_kv_leveled_backend ->
     lager:info("rt:set_backend(~p)", [Backend]),
     update_app_config(all, [{riak_kv, [{storage_backend, Backend}]}]),
     get_backends();
