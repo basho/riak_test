496f4bc351b6f35992dfb68f1d2740535dc8a443
Martin Sumner <martin.sumner@adaptip.co.uk>
2019-03-12 16:35:22 +0000

    Add 2i index test
    2i index entries can get large:
    
    2.0 and below - they could be arbitrarily large
    2.1 - 2.2.5 - they could only be as large as the unconfigurable receive buffer
    2.9 and above - they can be as large as the configurable receive buffer

--- tests/overlap_with_crdt_tag.erl
+++ tests/overlap_with_crdt_tag.erl
@@ -1,7 +1,5 @@
 %% -------------------------------------------------------------------
 %%
-%% Copyright (c) 2013 Basho Technologies, Inc.
-%%
 %% This file is provided to you under the Apache License,
 %% Version 2.0 (the "License"); you may not use this file
 %% except in compliance with the License.  You may obtain
